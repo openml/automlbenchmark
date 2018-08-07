aaa465f8d32367a0f9509d2d1a83ae486846012c
Janek Thomas <janek.thomas@web.de>
2018-08-07 15:23:46 -0700

    create container Class

--- aws/run_container_on_aws.py
+++ aws/run_container_on_aws.py
@@ -4,34 +4,79 @@ import boto3
 from time import sleep
 from os import popen
 
-key = "laptop" #ssh key
-sec = "launch-wizard-7" # security group
-instance = "t2.micro" # instance type
-image = "ami-58d7e821" # aws instance image
-dockerImage = "jnkthms/rf" # docker image
-openmlid = 1
-runtime = 1
-cores = 1
-apikey = popen("cat ~/.openml/config | grep apikey").read().split("=")[1][:-2] # openml apikey
+class AwsDockerOMLRun:
 
+  setup = '#!/bin/bash\napt-get update\napt-get install apt-transport-https ca-certificates curl software-properties-common\ncurl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -\nadd-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"\napt-get update\napt-get install -y docker-ce\nusermod -aG docker $USER\ndocker run --rm'
+  instance = None
 
-setup = '#!/bin/bash\napt-get update\napt-get install apt-transport-https ca-certificates curl software-properties-common\ncurl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -\nadd-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"\napt-get update\napt-get install -y docker-ce\nusermod -aG docker $USER\ndocker run --rm'
-setup = " ".join([setup, dockerImage, str(openmlid), str(runtime), str(cores), apikey, ">> /home/ubuntu/file.csv"])
-ec2 = boto3.resource("ec2")
+  def __init__(self, ssh_key, sec_group, aws_instance_type, aws_instance_image, docker_image, openml_id, runtime, cores, openml_apikey):
+    self.ssh_key = ssh_key
+    self.sec_group = sec_group
+    self.aws_instance_type = aws_instance_type
+    self.aws_instance_image = aws_instance_image
+    self.docker_image = docker_image
+    self.openml_id = openml_id
+    self.runtime = runtime
+    self.cores = cores
+    self.openml_apikey = openml_apikey
+    self.ec2_resource = boto3.resource("ec2")
 
-inst = ec2.create_instances(
-  ImageId = image,
-  MinCount = 1,
-  MaxCount = 1,
-  InstanceType = instance,
-  KeyName = key,
-  SecurityGroupIds = [sec],
-  UserData = setup
-)[0]
+  def createInstanceRun(self):
+    setup = " ".join([self.setup, self.docker_image, str(self.openml_id), str(self.runtime), str(self.cores), self.openml_apikey, ">> /home/ubuntu/result"])
+    if self.instance is not None:
+      print("Instance already exists, terminate existing instance")
+      self.terminateInstance()
 
-sleep(300 + runtime) # setup time + runtime
-inst.load() # update instance state in boto3
+    self.instance = self.ec2_resource.create_instances(
+      ImageId = self.aws_instance_image,
+      MinCount = 1,
+      MaxCount = 1,
+      InstanceType = self.aws_instance_type,
+      KeyName = self.ssh_key,
+      SecurityGroupIds = [self.sec_group],
+      UserData = setup)[0]
 
-res = popen('scp "%s:%s" "%s"' % ("ubuntu@" + inst.public_ip_address, "~/file.csv", "/dev/stdout")).read()
+  def terminateInstance(self):
+    if self.instance is not None:
+      termination_result = self.instance.terminate()
+      if not termination_result["TerminatingInstances"][0]["CurrentState"]["Code"] == 32:
+        print("Instance could not be terminated!")
+      else:
+        print("Termination successful")
 
-inst.terminate()
+  def getResult(self):
+    if self.instance is None:
+      print("Not instance created, run createInstanceRun first")
+      return None
+    self.instance.load()
+    if not self.instance.state["Name"] == "running":
+      print("Instance %s" % (self.instance.state["Name"]))
+    try:
+      res = popen('scp "ubuntu@%s:~/result" "/dev/stdout"' % (self.instance.public_ip_address)).read()
+      return res
+    except:
+      print("Run not finished")
+
+  def __del__(self):
+    self.terminateInstance()
+
+if __name__ == "main":
+  key = "laptop" #ssh key
+  sec = "launch-wizard-7" # security group
+  instance = "t2.micro" # instance type
+  image = "ami-58d7e821" # aws instance image
+  dockerImage = "jnkthms/rf" # docker image
+  openmlid = 1
+  runtime = 1
+  cores = 1
+  apikey = popen("cat ~/.openml/config | grep apikey").read().split("=")[1][:-2] # openml apikey
+
+  run = AwsDockerOMLRun(ssh_key = key, sec_group = sec, aws_instance_type = instance,
+    aws_instance_image = image, docker_image = dockerImage, openml_id = openmlid, runtime = runtime, cores = cores,
+    openml_apikey = apikey)
+
+  run.createInstanceRun()
+  for i in range(100):
+    sleep(5)
+    run.getResult()
+  run.terminateInstance()
