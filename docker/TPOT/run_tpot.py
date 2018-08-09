05d6f35cbf77f33091458f2bfe8f0c5145191751
PGijsbers <p.gijsbers@tue.nl>
2018-08-08 18:15:12 -0700

    Updated TPOT script work with preloaded data.

--- docker/TPOT/run_tpot.py
+++ docker/TPOT/run_tpot.py
@@ -1,42 +1,38 @@
 import sys
 
+import arff
 import numpy as np
-import openml
-from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
 from tpot import TPOTClassifier
 
 if __name__ == '__main__':
-    task_id = sys.argv[1]
-    runtime_seconds = sys.argv[2]
-    number_cores = int(sys.argv[3])
-    openml.config.apikey = sys.argv[4]
+    train_data_path = "../common/train.arff"
+    test_data_path = "../common/test.arff"
 
-    print('Running TPOT for task {} with a maximum time of {}s on {} cores.'
-          .format(task_id, runtime_seconds, number_cores))
+    runtime_seconds = sys.argv[1]
+    number_cores = int(sys.argv[2])
+    performance_metric = sys.argv[3]
 
-    print('Downloading task.')
-    task = openml.tasks.get_task(task_id)
-    X, y = task.get_X_and_y()
+    print('Loading data.')
+    def get_X_y_from_arff(arff_file_path):
+        with open(arff_file_path, 'r') as arff_data_file:
+            data_arff = arff.load(arff_data_file)
+            data = data_arff['data']
+            data = np.asarray(data)
+            return data[:, :-1], data[:, -1]
+
+    X_train, y_train = get_X_y_from_arff(train_data_path)
+    X_test, y_test = get_X_y_from_arff(test_data_path)
+    X_train, X_test = X_train.astype(float), X_test.astype(float)
+
+    print('Running TPOT with a maximum time of {}s on {} cores, optimizing {}.'
+          .format(runtime_seconds, number_cores, performance_metric))
 
     print('Running model on task.')
-    runtime_min = (int(runtime_seconds)/60) / 10
-    runtime_min = 1
-    tpot = TPOTClassifier(n_jobs=number_cores, population_size=10, generations=2, verbosity=2)
-
-    repeats, folds, samples = task.get_split_dimensions()
-    evaluation_measure = 'accuracy' if task.evaluation_measure == 'predictive_accuracy' else task.evaluation_measure
-    scores = []
-    if repeats == 1:
-        cv_folds = [task.get_train_test_split_indices(fold=f, repeat=r) for r in range(repeats) for f in range(folds)]
-        for (train_idx, test_idx) in cv_folds:
-            try:
-                tpot.fit(X[train_idx, :], y[train_idx])
-            except KeyboardInterrupt:
-                pass
-            scores.append(tpot.score(X[test_idx, :], y[test_idx]))
-
-        # Because actual metric values are calculated on the server side of things, we actually need to do it manually here.
-        # Note that code below does not generalize to e.g. repeated CV.
-        print(np.mean(scores))
-    else:
-        print('no output.')
+    runtime_min = (int(runtime_seconds)/60)
+    print('ignoring runtime.')
+    tpot = TPOTClassifier(n_jobs=number_cores, population_size=10, generations=2, verbosity=2, scoring=performance_metric)
+    tpot.fit(X_train, y_train)
+    y_pred = tpot.predict(X_test)
+    print('Optimization was towards metric, but following score is always accuracy:')
+    print("THIS_IS_A_DUMMY_TOKEN " + str(accuracy_score(y_test, y_pred)))
