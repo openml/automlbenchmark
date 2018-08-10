13ea4830155141591aeaee4cb5e8e0561442877c
PGijsbers <p.gijsbers@tue.nl>
2018-08-09 18:55:24 -0700

    The AutoML directory probably changes less often, and is less intrusive to rebuild, so it should be layered after the package installs.

--- docker/generate_docker.sh
+++ docker/generate_docker.sh
@@ -4,11 +4,11 @@ DIRECTORY=$1
 # Copy the first layers.
 cat DockerHeader > $DIRECTORY/Dockerfile
 
-# Add the AutoML system
-echo "ADD $DIRECTORY /bench/automl" >> $DIRECTORY/Dockerfile
-
 # Copy the AutoML-specific layers.
 cat $DIRECTORY/CustomDockerCode >> $DIRECTORY/Dockerfile
+
+# Add the AutoML system
+echo "ADD $DIRECTORY /bench/automl" >> $DIRECTORY/Dockerfile
 
 # Append the ENTRY_POINT call.
 cat DockerFooter >> $DIRECTORY/Dockerfile
