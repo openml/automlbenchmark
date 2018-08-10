1f8743edf6417ce07bea461a3fc2a1600c9aee26
PGijsbers <p.gijsbers@tue.nl>
2018-08-09 19:33:03 -0700

    Predictions are now written to file instead of reported directly.

--- docker/RandomForest/randomforest.py
+++ docker/RandomForest/randomforest.py
@@ -7,12 +7,14 @@ from sklearn.metrics import accuracy_score
 
 train_data_path = "../common/train.arff"
 test_data_path = "../common/test.arff"
+output_path = "../common/predictions.csv"
 
 runtime_seconds = sys.argv[1]
 number_cores = int(sys.argv[2])
 performance_metric = sys.argv[3]
 
 print('Running RandomForest with a maximum time of {}s on {} cores.'.format(runtime_seconds, number_cores))
+print('We completely ignore the requirement to stay within the time limit.')
 print('We completely ignore the advice to optimize towards metric: {}.'.format(performance_metric))
 
 with open(train_data_path, 'r') as train_file:
@@ -28,11 +30,9 @@ with open(test_data_path, 'r') as test_file:
 print('Running model on task.')
 rfc = RandomForestClassifier(n_jobs=number_cores)
 rfc.fit(X_train, y_train)
-y_pred = rfc.predict(X_test)
+class_predictions = rfc.predict(X_test).reshape(-1, 1)
+class_probabilities = rfc.predict_proba(X_test)
+combined_predictions = np.hstack((class_probabilities, class_predictions)).astype(str)
 
-# Because actual metric values are calculated on the server side of things, we actually need to do it manually here.
-# Note that code below does not generalize to e.g. repeated CV.
-print("THIS_IS_A_DUMMY_TOKEN " + str(accuracy_score(y_test, y_pred)))
-
-
-
+print(combined_predictions)
+np.savetxt(output_path, combined_predictions, delimiter=',', fmt="%s")
