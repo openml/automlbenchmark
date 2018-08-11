be701117060b11330a6d60dd3a6d878f7b66d565
PGijsbers <p.gijsbers@tue.nl>
2018-08-10 18:22:51 -0700

    Allow a dataset to be added to the image.

--- docker/generate_docker.sh
+++ docker/generate_docker.sh
@@ -10,5 +10,10 @@ cat $DIRECTORY/CustomDockerCode >> $DIRECTORY/Dockerfile
 # Add the AutoML system
 echo "ADD $DIRECTORY /bench/automl" >> $DIRECTORY/Dockerfile
 
+if [ $# -eq 2 ]; then
+	echo "Adding dataset to docker file"
+	echo "ADD $2 /bench/automl/dataset" >> $DIRECTORY/Dockerfile
+fi
+
 # Append the ENTRY_POINT call.
 cat DockerFooter >> $DIRECTORY/Dockerfile
