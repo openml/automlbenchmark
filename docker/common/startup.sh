8056b6ae4486aca398b01d6259c78d2a89951578
PGijsbers <p.gijsbers@tue.nl>
2018-08-10 18:22:13 -0700

    Now changes call to load_data depending on if a task or datafile is specified.

--- docker/common/startup.sh
+++ docker/common/startup.sh
@@ -30,9 +30,15 @@ if [ -z ${apikey+x} ]; then echo "apikey is unset"; exit; fi
 if [ -z ${time_s+x} ]; then echo "time_s is unset"; exit; fi
 if [ -z ${n_cores+x} ]; then echo "n_cores is unset"; exit; fi
 if [ -z ${metric+x} ]; then echo "metric is unset"; exit; fi
-if [ -z ${task_id+x} ] && [ -z ${datafile+x} ]; then echo "Either task or datafile needs to be set"; exit; fi
 
-/venvs/setup/bin/python3 ./common/load_data.py -t $task_id -f $fold_n -a $apikey --train ./common/train.arff --test ./common/test.arff
+if [ ! -z ${task_id+x} ]; then
+  /venvs/setup/bin/python3 ./common/load_data.py -t $task_id -f $fold_n -a $apikey --train ./common/train.arff --test ./common/test.arff
+elif [ ! -z ${datafile+x} ]; then
+  echo "No task was specified. Data on disk will be used."
+  /venvs/setup/bin/python3 ./common/load_data.py -d /bench/automl/dataset -f $fold_n --train ./common/train.arff --test ./common/test.arff
+else
+  echo "Neither a task nor datafile was specified. Can not continue."; exit
+fi
 
 cd automl
 $start_call $time_s $n_cores $metric
