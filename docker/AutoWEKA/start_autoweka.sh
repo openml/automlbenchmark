3d4566e1586788013daf20ba1d21eb395b19cc79
PGijsbers <p.gijsbers@tue.nl>
2018-08-13 15:15:35 -0700

    Added Auto-WEKA framework (some settings still have to be clarified).

---
+++ docker/AutoWEKA/start_autoweka.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+time_s=$1
+n_cores=$2
+metric=$3
+let time_m=$time_s/60
+
+echo "Time is $time_s s or $time_m m."
+
+echo "Ignoring metric"
+echo "Set memlimit"
+java -cp autoweka.jar weka.classifiers.meta.AutoWEKAClassifier -t ../common/train.arff -T ../common/test.arff -memLimit 1024 -classifications "weka.classifiers.evaluation.output.prediction.CSV -distribution -file predictions.csv" -timeLimit $time_m -parallelRuns $n_cores
+# -metric $metric
+
+python3 reformat_output.py predictions.csv ../common/predictions.csv
