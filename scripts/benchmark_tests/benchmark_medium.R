5d789775dcf57a926265a9f690fb26652cc20c55
Janek Thomas <janek.thomas@web.de>
2018-08-16 14:41:49 -0700

    initial test benchmark for medium size datasts

---
+++ scripts/benchmark_tests/benchmark_medium.R
@@ -0,0 +1,73 @@
+library(batchtools)
+library(OpenML)
+library(mlr)
+library(mlrCPO)
+
+LOCAL = TRUE
+
+if (LOCAL) {
+  data.ids = c(61)
+  replications = 1L
+} else {
+  data.ids = c(1112, 1114, 1111, 40996, 40668, 23517, 23512, 4135, 1486, 41027, 1461, 151,
+  1590, 41167, 41126, 41169, 41168, 41137, 41165, 40685, 41159, 41161, 1216, 23513, 41150,
+  41138, 41162)
+  replications = 5L
+}
+
+
+
+lrns = list(
+  lrn.xgboost = cpoImpactEncodeClassif() %>>%
+    makeLearner("classif.xgboost", nrounds = 100, predict.type = "prob"),
+  lrn.ranger = cpoImputeAll(classes = list(numeric = imputeHist(), factor = imputeConstant("__MISS__"))) %>>%
+    makeLearner("classif.ranger", predict.type = "prob"),
+  lrn.rpart = cpoImpactEncodeClassif() %>>%
+    makeLearner("classif.rpart", predict.type = "prob"),
+  lrn.qda = cpoImpactEncodeClassif() %>>%
+    cpoImputeAll(classes = list(numeric = imputeHist(), factor = imputeConstant("__MISS__"))) %>>%
+    makeLearner("classif.qda", predict.type = "prob"),
+  lrn.baseline = makeLearner("classif.featureless", predict.type = "prob")
+)
+
+resampling = makeResampleDesc("Holdout", split = 0.5, stratify = TRUE)
+
+resources = list(
+  walltime = 3600L,
+  memory = 1024L * 62L,
+  ntasks = 1L,
+  ncpus = 28L,
+  nodes = 1L,
+  clusters = "mpp2"
+  )
+
+reg = makeExperimentRegistry("medium_openml", packages = c("OpenML", "mlr", "mlrCPO"))
+reg$default.resources = resources
+
+
+for(data.id in data.ids) {
+  addProblem(name = as.character(data.id),
+    data = convertOMLDataSetToMlr(getOMLDataSet(data.id)),
+    fun = function(job, data, res = resampling) makeResampleInstance(res, data))
+}
+
+addAlgorithm(name = "mlr",
+  fun = function(job, data, instance, lrn) {
+    if (length(getTaskClassLevels(data)) == 2)
+      measures = list(auc, logloss, acc, timetrain)
+    else
+      measures = list(multiclass.aunp, logloss, acc, timetrain)
+    resample(lrn, data, instance, measures, keep.pred = FALSE)
+  }
+)
+
+
+addExperiments(algo.designs = list(mlr = data.table(lrn = lrns)), repls = replications)
+summarizeExperiments()
+
+if (LOCAL) {
+  submitJobs()
+  getStatus()
+  x = unwrap(reduceResultsDataTable(fun = function(x) x$aggr))
+  res = x[getJobTable()[, algo.pars := lapply(algo.pars, function(x) x$lrn$id)]]
+}
