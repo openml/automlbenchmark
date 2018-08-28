768e41fa911f989e59a644db3716da0c084a9c51
PGijsbers <p.gijsbers@tue.nl>
2018-08-27 18:15:00 -0700

    Use argparse for benchmark.

--- benchmark.py
+++ benchmark.py
@@ -2,6 +2,7 @@
 
 from aws.AutoMLBenchmark import AutoMLBenchmark
 
+import argparse
 import sys
 import json
 from time import time
@@ -9,10 +10,18 @@ import pandas as pd
 
 aws_instance_image = "ami-0615f1e34f8d36362"
 
-framework = sys.argv[1]
-benchmark = sys.argv[2]
-mode = sys.argv[3]
-outfile = sys.argv[4] if len(sys.argv) > 4 else None
+parser = argparse.ArgumentParser()
+parser.add_argument('framework', type=str,
+                    help='The framework to evaluate as specified in /resources/frameworks.json')
+parser.add_argument('benchmark', type=str,
+                    help='The benchmark to run as specified in /resources/benchmarks.json')
+parser.add_argument('mode', type=str,
+                    help='The mode that specifies what backend is used (currently local or aws)')
+parser.add_argument('-o', '--outfile', dest='outfile', metavar='-o', default=None,
+                    help='Path where the output should be written.')
+parser.add_argument('-r', '--region', dest='region', metavar='-r', default=None,
+                    help='The region on which to run the benchmark when using AWS.')
+args = parser.parse_args()
 
 
 with open("resources/benchmarks.json") as file:
@@ -22,26 +31,28 @@ with open("resources/frameworks.json") as file:
     frameworks = json.load(file)
 
 
-bench = AutoMLBenchmark(benchmarks=benchmarks[benchmark], framework=frameworks[framework])#, region_name=outfile)
+bench = AutoMLBenchmark(benchmarks=benchmarks[args.benchmark], framework=frameworks[args.framework], region_name=args.region)
 
-print("Running `%s` on `%s` benchmarks in `%s` mode" % (framework, benchmark, mode))
+print("Running `%s` on `%s` benchmarks in `%s` mode" % (args.framework, args.benchmark, args.mode))
 
-if mode == "aws":
+if args.mode == "aws":
     bench.update_docker_container(upload=True)
     res = bench.run_aws(aws_instance_image)
-elif mode == "local":
+elif args.mode == "local":
     bench.update_docker_container(upload=False)
     res = bench.run_local()
+else:
+    raise ValueError('mode must be one of \'aws\' or \'local\'.')
 
-if outfile is not None:
-    with open(outfile, "a") as file:
+if args.outfile is not None:
+    with open(args.outfile, "a") as file:
         for r in res:
             file.writelines(",".join([r["benchmark_id"],
-                                     framework,
-                                     frameworks[framework]["version"],
+                                     args.framework,
+                                     frameworks[args.framework]["version"],
                                      str(r["fold"]),
                                      str(r["result"]),
-                                     mode,
+                                     args.mode,
                                      str(int(time()))]) + "\n")
 else:
     print(pd.DataFrame(res))
