bd1c1c33d2868518f1d798ed41e232ae12574c9b
Janek Thomas <janek.thomas@web.de>
2018-08-29 18:52:26 -0700

    create mapping for categs

--- docker/autosklearn/run_autosklearn.py
+++ docker/autosklearn/run_autosklearn.py
@@ -1,14 +1,13 @@
 from autosklearn.classification import AutoSklearnClassifier
 import autosklearn.metrics
 from sklearn.metrics import accuracy_score, roc_auc_score, log_loss
-from numpy import unique
 import time
 
 import sys
 sys.path.append('/bench/common')
-import common_code
 
 if __name__ == '__main__':
+    import common_code
 
     runtime_seconds = int(sys.argv[1])
     number_cores = int(sys.argv[2])
@@ -20,27 +19,26 @@ if __name__ == '__main__':
     elif performance_metric == "auc":
         performance_metric = autosklearn.metrics.roc_auc
     elif performance_metric == "logloss":
-        performance_metric = autosklearn.metrics.log_loss   
+        performance_metric = autosklearn.metrics.log_loss
     else:
         # TO DO: Figure out if we are going to blindly pass metrics through, or if we use a strict mapping
         print('Performance metric, {}, not supported.'.format(performance_metric))
 
-    X_train, y_train = common_code.get_X_y_from_arff(common_code.TRAIN_DATA_PATH)
-    X_test, y_test = common_code.get_X_y_from_arff(common_code.TEST_DATA_PATH)
-    X_train, X_test = X_train.astype(float), X_test.astype(float)
-
+    X_train, y_train, mapping = common_code.get_X_y_from_arff(common_code.TRAIN_DATA_PATH)
+    X_test, y_test, _ = common_code.get_X_y_from_arff(common_code.TEST_DATA_PATH, mapping)
+    categs = ["Categorical" if col in mapping.keys() else "Numerical" for col in range(X_train.shape[1])]
     # Set resources based on datasize
     print('ignoring n_cores.')
     # If small data:
     if len(y_train) <= 20000:
-        #number_cores = 8
-        ml_memory_limit = 16000 #16GB
+        # number_cores = 8
+        ml_memory_limit = 16000  # 16GB
     elif len(y_train) <= 200000:
-        #number_cores = 16
-        ml_memory_limit = 64000 #64GB
+        # number_cores = 16
+        ml_memory_limit = 64000  # 64GB
     else:
-        #number_cores = 64
-        ml_memory_limit = 64000 #64GB
+        # number_cores = 64
+        ml_memory_limit = 64000  # 64GB
 
     print('Running auto-sklearn with a maximum time of {}s on {} cores, optimizing {}.'
           .format(runtime_seconds, number_cores, performance_metric))
@@ -48,13 +46,13 @@ if __name__ == '__main__':
     print('Using meta-learned initialization, which might be bad (leakage).')
     # TO DO: Do we need to set per_run_time_limit too?
     starttime = time.time()
-    auto_sklearn = AutoSklearnClassifier(time_left_for_this_task=runtime_seconds, \
-        ml_memory_limit=ml_memory_limit)
+    auto_sklearn = AutoSklearnClassifier(time_left_for_this_task=runtime_seconds,
+                                         ml_memory_limit=ml_memory_limit,
+                                         feat_type=categs)
     auto_sklearn.fit(X_train, y_train, metric=performance_metric)
     actual_runtime_min = (time.time() - starttime)/60.0
     print('Requested training time (minutes): ' + str((runtime_seconds/60.0)))
-    print('Actual training time (minutes): ' + str(actual_runtime_min))    
-
+    print('Actual training time (minutes): ' + str(actual_runtime_min))
 
     # Convert output to strings for classification
     print('Predicting on the test set.')
@@ -62,10 +60,10 @@ if __name__ == '__main__':
     class_probabilities = auto_sklearn.predict_proba(X_test)
 
     print('Optimization was towards metric, but following score is always accuracy:')
-    print("Accuracy: " + str(accuracy_score(y_test, class_predictions)))  
+    print("Accuracy: " + str(accuracy_score(y_test, class_predictions)))
 
     if class_probabilities.shape[1] == 2:
-        auc = roc_auc_score(y_true=y_test.astype(int), y_score=class_probabilities[:,1])
+        auc = roc_auc_score(y_true=y_test.astype(int), y_score=class_probabilities[:, 1])
         print("AUC: " + str(auc))
     else:
         logloss = log_loss(y_true=y_test.astype(int), y_pred=class_probabilities)
