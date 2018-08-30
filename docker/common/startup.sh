ea39f9774fc1ca4bdc837cb91b39221be3a437d4
PGijsbers <p.gijsbers@tue.nl>
2018-08-29 17:28:01 -0700

    Allow memory option. Memory passed to start_call.

--- docker/common/startup.sh
+++ docker/common/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # http://wiki.bash-hackers.org/howto/getopts_tutorial
-while getopts :c:t:d:f:a:s:p:m: opt; do
+while getopts :c:t:d:f:a:s:p:m:g: opt; do
   case $opt in
     c)
 	  start_call=$OPTARG;;
@@ -19,6 +19,8 @@ while getopts :c:t:d:f:a:s:p:m: opt; do
 	  n_cores=$OPTARG;;
 	m)
 	  metric=$OPTARG;;
+	g)
+	  allowedMemGB=$OPTARG;;
 	\?)
 	  echo "Invalid option: -$OPTARG";;
   esac	  
@@ -29,6 +31,23 @@ if [ -z ${fold_n+x} ]; then echo "fold_n is unset"; exit; fi
 if [ -z ${time_s+x} ]; then echo "time_s is unset"; exit; fi
 if [ -z ${n_cores+x} ]; then echo "n_cores is unset"; exit; fi
 if [ -z ${metric+x} ]; then echo "metric is unset"; exit; fi
+
+# We set the memory limit, if memory is overspecified we only give warnings, default is to leave 2GB for OS.
+totalMemMB=$(free -m|awk '/^Mem:/{print $2}')
+if [ ! -z ${allowedMemGB+x} ]; then
+  allowedMemMB=$(( $allowedMemGB * 1024 ))
+  if [ $allowedMemMB -gt $totalMemMB ]; then
+    echo "WARNING: Specified memory exceeds system memory."
+	echo "Specified: " $allowedMemMB "MB, Sytem: " $totalMemMB "MB."
+  elif [ $allowedMemMB -gt $(( $totalMemMB - 2048 )) ]; then
+    echo "WARNING: Specified memory within 2Gb of system memory."
+	echo "We encourage a 2Gb buffer, because otherwise OS memory usage might interfere with the AutoML framework."
+	echo "Specified: " $allowedMemMB "MB, Sytem: " $totalMemMB "MB."    
+  fi
+else
+  allowedMemMB=$(( $totalMemMB - 2048 ))
+  echo "Memory set to use all but 2GB (default): " $allowedMemMB "MB."
+fi
 
 if [ ! -z ${task_id+x} ]; then
   if [ ! -z ${apikey+x} ]; then
@@ -44,7 +63,7 @@ else
 fi
 
 cd automl
-$start_call $time_s $n_cores $metric
+$start_call $time_s $n_cores $metric $allowedMemMB
 cd ..
 
 /venvs/setup/bin/python3 -Wignore ./common/evaluate.py ./common/test.arff ./common/predictions.csv $metric
