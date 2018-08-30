5538b47304df15ea096b4097a752091975f514ef
PGijsbers <p.gijsbers@tue.nl>
2018-08-29 18:53:23 -0700

    Specify memory.

--- docker/AutoWEKA/run_autoweka.sh
+++ docker/AutoWEKA/run_autoweka.sh
@@ -2,6 +2,7 @@
 time_s=$1
 n_cores=$2
 metric=$3
+max_memory_mb=$4
 let time_m=$time_s/60
 
 if [ $metric == acc ] ; then
@@ -12,7 +13,7 @@ elif [ $metric == logloss ] ; then
   metric=kBInformation
 fi
 
-echo "TODO: set memlimit"
-java -cp autoweka.jar weka.classifiers.meta.AutoWEKAClassifier -t ../common/train.arff -T ../common/test.arff -memLimit 1024 -classifications "weka.classifiers.evaluation.output.prediction.CSV -distribution -file predictions.csv" -timeLimit $time_m -parallelRuns $n_cores -metric $metric
+echo $max_memory_mb "Mb"
+java -cp autoweka.jar weka.classifiers.meta.AutoWEKAClassifier -t ../common/train.arff -T ../common/test.arff -memLimit $max_memory_mb -classifications "weka.classifiers.evaluation.output.prediction.CSV -distribution -file predictions.csv" -timeLimit $time_m -parallelRuns $n_cores -metric $metric
 
 python3 reformat_output.py predictions.csv ../common/predictions.csv
