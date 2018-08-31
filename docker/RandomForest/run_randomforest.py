10860e023bc26602e8d4f2f24ed95853cd0c32a6
PGijsbers <p.gijsbers@tue.nl>
2018-08-30 17:29:34 -0700

    Update loading.

--- docker/RandomForest/run_randomforest.py
+++ docker/RandomForest/run_randomforest.py
@@ -10,8 +10,8 @@ runtime_seconds = sys.argv[1]
 number_cores = int(sys.argv[2])
 performance_metric = sys.argv[3]
 
-X_train, y_train = common_code.get_X_y_from_arff(common_code.TRAIN_DATA_PATH)
-X_test, y_test = common_code.get_X_y_from_arff(common_code.TEST_DATA_PATH)
+X_train, y_train, mapping = common_code.get_X_y_from_arff(common_code.TRAIN_DATA_PATH)
+X_test, y_test, _ = common_code.get_X_y_from_arff(common_code.TEST_DATA_PATH, mapping)
 
 # Impute any missing data (can test using -t 146606)
 imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
@@ -26,7 +26,7 @@ print('Running RandomForest with a maximum time of {}s on {} cores.'.format(runt
 print('We completely ignore the requirement to stay within the time limit.')
 print('We completely ignore the advice to optimize towards metric: {}.'.format(performance_metric))
 
-rfc = RandomForestClassifier(n_jobs=number_cores, n_estimators=100)
+rfc = RandomForestClassifier(n_jobs=number_cores, n_estimators=2000)
 rfc.fit(X_train, y_train)
 class_predictions = rfc.predict(X_test)
 class_probabilities = rfc.predict_proba(X_test)
