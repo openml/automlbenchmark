03f41494631dac1b01924c73b343fca3f3a5726f
Sebastien Poirier <sebastien@h2o.ai>
2018-12-04 21:58:10 +0100

    fixed autoweka setup

--- automl/frameworks/AutoWEKA/setup.sh
+++ automl/frameworks/AutoWEKA/setup.sh
@@ -10,5 +10,5 @@ DOWNLOAD_DIR="./libs"
 TARGET_DIR="$DOWNLOAD_DIR/autoweka"
 if [[ ! -e "$TARGET_DIR" ]]; then
     wget http://www.cs.ubc.ca/labs/beta/Projects/autoweka/$AUTOWEKA_ARCHIVE -P $DOWNLOAD_DIR
-    unzip $DOWNLOAD_DIR/$AUTOWEKA_ARCHIVE -d TARGET_DIR
+    unzip $DOWNLOAD_DIR/$AUTOWEKA_ARCHIVE -d $TARGET_DIR
 fi
