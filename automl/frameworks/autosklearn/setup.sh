007acc93e809457e007df18ea17cbf54abcfdcb9
Sebastien Poirier <sebastien@h2o.ai>
2018-12-14 01:21:30 +0100

    fixing autosklearn setup

--- automl/frameworks/autosklearn/setup.sh
+++ automl/frameworks/autosklearn/setup.sh
@@ -3,5 +3,5 @@
 if [[ -x "$(command -v apt-get)" ]]; then
     apt-get install -y build-essential swig
 fi
-curl https://raw.githubusercontent.com/automl/auto-sklearn/master/requirements.txt | xargs -n 1 -L 1 PIP install
+curl https://raw.githubusercontent.com/automl/auto-sklearn/master/requirements.txt | while read -r i; do PIP install "$i"; done
 PIP install --no-cache-dir -r automl/frameworks/autosklearn/py_requirements.txt
