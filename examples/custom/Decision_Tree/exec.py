afcd4e2be556e3d4f1c9cde85410aba0e905a187
Sebastien Poirier <sebastien@h2o.ai>
2019-01-31 17:30:16 +0100

    fixed custom decsition tree example

--- examples/custom/Decision_Tree/exec.py
+++ examples/custom/Decision_Tree/exec.py
@@ -3,6 +3,7 @@ from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 
 from automl.benchmark import TaskConfig
 from automl.data import Dataset
+from automl.datautils import impute
 from automl.results import save_predictions_to_file
 
 log = logging.getLogger(__name__)
@@ -13,16 +14,19 @@ def run(dataset: Dataset, config: TaskConfig):
 
     is_classification = config.type == 'classification'
 
+    X_train, X_test = impute(dataset.train.X, dataset.test.X)
+    y_train, y_test = dataset.train.y, dataset.test.y
+
     estimator = DecisionTreeClassifier if is_classification else DecisionTreeRegressor
     predictor = estimator(**config.framework_params)
 
-    predictor.fit(dataset.train.X, dataset.train.y)
-    predictions = predictor.predict(dataset.test.X)
-    probabilities = predictor.predict_proba(dataset.test.X) if is_classification else None
+    predictor.fit(X_train, y_train)
+    predictions = predictor.predict(X_test)
+    probabilities = predictor.predict_proba(X_test) if is_classification else None
 
     save_predictions_to_file(dataset=dataset,
                              output_file=config.output_predictions_file,
                              probabilities=probabilities,
                              predictions=predictions,
-                             truth=dataset.test.y)
+                             truth=y_test)
 
