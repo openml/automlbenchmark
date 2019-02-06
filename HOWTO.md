839a0b8802add85034c20b984f3c1db776476212
Sebastien Poirier <sebastien@h2o.ai>
2019-02-06 22:31:58 +0100

    added constant franework entry to quickly test encoding on new datasets

--- HOWTO.md
+++ HOWTO.md
@@ -3,6 +3,13 @@
 ## Run a benchmark
 see [README.md]
 
+### Verify a dataset
+
+- create a small benchmark file including the dataset.
+- run `constantpredictor` on the dataset.
+- run `constantpredictor_enc` on the dataset for frameworks that require encoded data.
+- check results for errors.
+
 ### Run a framework with different (hyper-)parameters
 
 ### Add a new benchmark
--- frameworks/constantpredictor/exec.py
+++ frameworks/constantpredictor/exec.py
@@ -16,13 +16,20 @@ def run(dataset: Dataset, config: TaskConfig):
     is_classification = config.type == 'classification'
     predictor = DummyClassifier(strategy='prior') if is_classification else DummyRegressor(strategy='median')
 
-    predictor.fit(dataset.train.X, dataset.train.y)
-    predictions = predictor.predict(dataset.test.X)
-    probabilities = predictor.predict_proba(dataset.test.X) if is_classification else None
+    encode = config.framework_params['encode'] if 'encode' in config.framework_params else False
+    X_train = dataset.train.X_enc if encode else dataset.train.X
+    y_train = dataset.train.y_enc if encode else dataset.train.y
+    X_test = dataset.test.X_enc if encode else dataset.test.X
+    y_test = dataset.test.y_enc if encode else dataset.test.y
+
+    predictor.fit(X_train, y_train)
+    predictions = predictor.predict(X_test)
+    probabilities = predictor.predict_proba(X_test) if is_classification else None
 
     save_predictions_to_file(dataset=dataset,
                              output_file=config.output_predictions_file,
                              probabilities=probabilities,
                              predictions=predictions,
-                             truth=dataset.test.y)
+                             truth=y_test,
+                             target_is_encoded=encode)
 
