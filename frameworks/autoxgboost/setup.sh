bd10d5c955ec1153d4540aa220e6837fca9aea9e
Coorsaa <stefan.coors@gmx.net>
2019-08-14 17:29:46 +0200

    added requirements

--- frameworks/autoxgboost/setup.sh
+++ frameworks/autoxgboost/setup.sh
@@ -8,6 +8,17 @@ if [[ -x "$(command -v apt-get)" ]]; then
     SUDO add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu bionic-cran35/'
     SUDO apt-get update
     SUDO apt-get install -y r-base r-base-dev
+
+    SUDO apt-get install default-jre
+    SUDO apt-get install default-jdk
+    SUDO R CMD javareconf
+    SUDO apt-get install r-cran-rjava
+    SUDO apt-get install libgdal-dev libproj-dev
+
+    SUDO apt-get install libssl-dev
+    SUDO apt-get install libcurl4-openssl-dev
+    SUDO apt-get install libcairo2-dev
+    SUDO apt-get install libudunits2-dev
 fi
 #PIP install --no-cache-dir -r $HERE/requirements.txt
 
