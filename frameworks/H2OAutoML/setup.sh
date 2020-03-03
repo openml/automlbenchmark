55b80dcfabd8a205f8980e3b4a279468428bf90b
Sebastien Poirier <sebastien@h2o.ai>
2020-03-03 16:38:25 +0100

    upgraded default h2o version to latest 3.28.0.4

--- frameworks/H2OAutoML/setup.sh
+++ frameworks/H2OAutoML/setup.sh
@@ -9,21 +9,25 @@ if [[ -x "$(command -v apt-get)" ]]; then
     SUDO apt-get install -y openjdk-8-jdk
 fi
 PIP install --no-cache-dir -r $HERE/requirements.txt
-if [[ "$VERSION" = "yau" || -z "$VERSION" ]]; then
-    echo "installing H2O-3 yau"
-    PIP install --no-cache-dir -U http://h2o-release.s3.amazonaws.com/h2o/rel-yau/11/Python/h2o-3.26.0.11-py2.py3-none-any.whl
+
+if [[ "$VERSION" = "yu" || -z "$VERSION" ]]; then
+    h2o_package="http://h2o-release.s3.amazonaws.com/h2o/rel-yu/4/Python/h2o-3.28.0.4-py2.py3-none-any.whl"
+elif [[ "$VERSION" = "yau" ]]; then
+    h2o_package="http://h2o-release.s3.amazonaws.com/h2o/rel-yau/11/Python/h2o-3.26.0.11-py2.py3-none-any.whl"
 elif [[ "$VERSION" = "yates" ]]; then
-    echo "installing H2O-3 yates"
-    PIP install --no-cache-dir -U http://h2o-release.s3.amazonaws.com/h2o/rel-yates/5/Python/h2o-3.24.0.5-py2.py3-none-any.whl
+    h2o_package="http://h2o-release.s3.amazonaws.com/h2o/rel-yates/5/Python/h2o-3.24.0.5-py2.py3-none-any.whl"
 elif [[ "$VERSION" = "xu" ]]; then
-    echo "installing H2O-3 xu"
-    PIP install --no-cache-dir -U http://h2o-release.s3.amazonaws.com/h2o/rel-xu/6/Python/h2o-3.22.1.6-py2.py3-none-any.whl
+    h2o_package="http://h2o-release.s3.amazonaws.com/h2o/rel-xu/6/Python/h2o-3.22.1.6-py2.py3-none-any.whl"
 elif [[ "$VERSION" = "xia" ]]; then
-    echo "installing H2O-3 xia"
-    PIP install --no-cache-dir -U http://h2o-release.s3.amazonaws.com/h2o/rel-xia/5/Python/h2o-3.22.0.5-py2.py3-none-any.whl
+    h2o_package="http://h2o-release.s3.amazonaws.com/h2o/rel-xia/5/Python/h2o-3.22.0.5-py2.py3-none-any.whl"
 elif [[ "$VERSION" = "wright" ]]; then
-    echo "installing H2O-3 wright"
-    PIP install --no-cache-dir -U http://h2o-release.s3.amazonaws.com/h2o/rel-wright/10/Python/h2o-3.20.0.10-py2.py3-none-any.whl
+    h2o_package="http://h2o-release.s3.amazonaws.com/h2o/rel-wright/10/Python/h2o-3.20.0.10-py2.py3-none-any.whl"
+fi
+
+if [[ -n "$h2o_package" ]]; then
+    echo "installing H2O-3 $VERSION"
+    PIP install --no-cache-dir -U $h2o_package
 else
     echo "not installing any H2O release version"
 fi
+
