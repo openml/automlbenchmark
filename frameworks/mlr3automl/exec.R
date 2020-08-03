04941f686b12cf59b57265b9d24b0dd426777818
a-hanf <alexanderhanf@gmx.de>
2020-08-03 13:50:23 +0200

    add mlr3automl to benchmark

---
+++ frameworks/mlr3automl/exec.R
@@ -0,0 +1,51 @@
+library(mlr3)
+library(mlr3tuning)
+library(mlr3pipelines)
+library(mlr3automl)
+library(farff)
+
+run <- function(train_file, test_file, target.index, type, output_predictions_file, cores, time.budget) {
+  train <- farff::readARFF(train_file)
+  colnames(train) <- make.names(colnames(train))
+  target <- colnames(train)[target.index]
+
+  test <- farff::readARFF(test_file)
+  colnames(test) <- make.names(colnames(test))
+
+  # TODO: missing data should be handled in mlr3automl itself
+  missing_cols <- names(which(sapply(train, anyNA)))
+  missing_cols <- c(missing_cols, names(which(sapply(test, anyNA))))
+
+  train <- train[, !(colnames(train) %in% missing_cols)]
+  test <- test[, !(colnames(test) %in% missing_cols)]
+
+
+  if (type == "classification") {
+    train <- TaskClassif$new("benchmark_train", backend = train, target = target)
+    test <- TaskClassif$new("benchmark_test", backend = test, target = target)
+  } else if (type == "regression") {
+    train <- TaskRegr$new("benchmark_train", backend = train, target = target)
+    test <- TaskRegr$new("benchmark_test", backend = test, target = target)
+  } else {
+    stop("Task type not supported!")
+  }
+
+  model <- AutoML(train, terminator = trm("run_time", secs = time.budget - 15))
+  model$train()
+  preds <- model$predict(test)
+  preds <- cbind(preds$data$prob, preds$data$tab)
+  names(preds)[names(preds) == "response"] <- "predictions"
+
+  if (type == "classification") {
+    names(preds) <- sub("^prob.", "", names(preds))
+  }
+
+  preds[, 'row_id'] <- NULL
+  write.table(preds, file = output_predictions_file,
+    row.names = FALSE, col.names = TRUE,
+    sep = ",", quote = FALSE
+  )
+}
+
+# args = commandArgs(trailingOnly=TRUE)
+# run(args[1], args[2], args[3], as.integer(args[4]))
---
+++ frameworks/mlr3automl/exec.py
@@ -0,0 +1,35 @@
+import logging
+import os
+
+from amlb.benchmark import TaskConfig
+from amlb.data import Dataset
+from amlb.utils import dir_of, run_cmd, Timer
+
+log = logging.getLogger(__name__)
+
+
+def run(dataset: Dataset, config: TaskConfig):
+    #TODO: use rpy2 instead? not necessary here though as the call is very simple
+    log.info("\n**** mlr3automl (R) ****\n")
+
+    is_classification = config.type == 'classification'
+
+    here = dir_of(__file__)
+
+    with Timer() as training:
+        run_cmd(r"""Rscript --vanilla -e "source('{script}'); run('{train}', '{test}', target.index = {target_index}, '{type}', '{output}', {cores}, time.budget = {time_budget})" """.format(
+            script=os.path.join(here, 'exec.R'),
+            train=dataset.train.path,
+            test=dataset.test.path,
+            target_index=dataset.target.index+1,
+            type=config.type,
+            output=config.output_predictions_file,
+            cores=config.cores,
+            time_budget=config.max_runtime_seconds
+        ), _live_output_=True)
+
+    log.info("Predictions saved to %s", config.output_predictions_file)
+
+    return dict(
+      training_duration=training.duration
+    )
