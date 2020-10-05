0e59c2442a3010ec4909149ddbbd71f117573388
PGijsbers <p.gijsbers@tue.nl>
2020-10-05 16:56:45 +0200

    Rename file/functions for clarity

--- tests/resources/test_framework_definition_processing.py
+++ tests/resources/test_framework_definition_processing.py
@@ -1,30 +1,19 @@
 import pytest
 from amlb.utils import Namespace
-from amlb.resources import load_and_merge_framework_definitions, \
-    remove_frameworks_with_unknown_parent, remove_self_reference_extensions, \
-    add_and_normalize_names, load_framework_definitions, from_config, \
-    autocomplete_definition, add_default_module, \
-    add_default_version, add_default_setup_args, \
-    add_default_setup_script, Resources, add_default_setup_cmd, add_default_params, \
-    add_default_image, add_defaults_to_frameworks, \
+from amlb.resources import remove_frameworks_with_unknown_parent, \
+    remove_self_reference_extensions, \
+    add_and_normalize_names,\
     update_frameworks_with_parent_definitions, find_all_parents, \
     sanitize_and_add_defaults
 
-directory_aliases = [
-    ("input", "my_input"),
-    ("output", "my_output"),
-    ("user", "my_user_dir"),
-    ("root", "my_root_dir"),
-]
 
-
-def test_remove_frameworks_with_unknown_parent_removes_one():
+def test_remove_frameworks_with_unknown_parent_removes_framework_with_unknown_parent():
     f = Namespace(dummy=Namespace(name="dummy", extends="does_not_exist"))
     remove_frameworks_with_unknown_parent(f)
     assert "dummy" not in f
 
 
-def test_remove_frameworks_with_unknown_parent_removes_none():
+def test_remove_frameworks_with_unknown_parent_keeps_children_with_known_parents():
     f = Namespace(
         dummy=Namespace(name="dummy", extends="does_exist"),
         does_exist=Namespace(name="does_exist"),
@@ -34,13 +23,13 @@ def test_remove_frameworks_with_unknown_parent_removes_none():
     assert "does_exist" in f
 
 
-def test_remove_self_reference_extensions_remove_one():
+def test_remove_self_reference_extensions_removes_self_reference():
     f = Namespace(dummy=Namespace(name="dummy", extends="dummy"))
     remove_self_reference_extensions(f)
     assert f.dummy.extends is None
 
 
-def test_remove_self_reference_extensions_remove_none():
+def test_remove_self_reference_extensions_does_not_remove_reference_to_other():
     f = Namespace(dummy=Namespace(name="dummy", extends="some_other_framework"))
     remove_self_reference_extensions(f)
     assert f.dummy.extends is "some_other_framework"
@@ -53,12 +42,17 @@ def test_add_and_normalize_names_adds_name():
     assert f.dummy.name == "dummy"
 
 
-def test_add_and_normalize_names_name_is_normalized():
+def test_add_and_normalize_names_converts_name_to_lower_case():
     f = Namespace(Dummy=Namespace())
     add_and_normalize_names(f)
     assert "dummy" in f, "The 'Dummy' entry should be in all lower case."
-    assert "Dummy" not in f, "The old name should be invalid."
     assert f.dummy.name == "dummy"
+
+
+def test_add_and_normalize_names_original_removed_for_normalized_framework():
+    f = Namespace(Dummy=Namespace())
+    add_and_normalize_names(f)
+    assert "Dummy" not in f, "The old name should be invalid."
 
 
 def test_add_and_normalize_names_extension_is_normalized():
@@ -67,7 +61,7 @@ def test_add_and_normalize_names_extension_is_normalized():
     assert f.dummy.extends == "anotherdummy"
 
 
-def test_find_all_parents_no_parent():
+def test_find_all_parents_returns_empty_list_if_framework_has_no_parent():
     frameworks = Namespace(
         gama=Namespace(name="gama", version="latest", description="flexible automl"),
         h2o_automl=Namespace(name="h2o", version="1.3"),
@@ -77,7 +71,7 @@ def test_find_all_parents_no_parent():
 
 
 @pytest.mark.parametrize("framework", ["gama", "h2o_automl"])
-def test_find_all_parents_one_parent(framework):
+def test_find_all_parents_returns_parent_of_framework_with_single_parent(framework):
     frameworks = Namespace(
         gama=Namespace(name="gama", version="latest", description="flexible automl"),
         gama_old=Namespace(name="gama_20.1.0", version="20.1.0", extends="gama"),
@@ -89,7 +83,7 @@ def test_find_all_parents_one_parent(framework):
 
 
 @pytest.mark.parametrize("framework", ["gama", "h2o_automl"])
-def test_find_all_parents_two_parents(framework):
+def test_find_all_parents_returns_frameworks_closest_first_if_two_parents(framework):
     frameworks = Namespace(
         gama=Namespace(name="gama", version="latest", description="flexible automl"),
         gama_old=Namespace(name="gama_20.1.0", version="20.1.0", extends="gama"),
@@ -102,13 +96,22 @@ def test_find_all_parents_two_parents(framework):
     assert parents == [frameworks[f"{framework}_old"], frameworks[framework]]
 
 
-def test_update_frameworks_with_parent_definitions_adds_parent_yaml():
+def test_update_frameworks_with_parent_definitions_add_missing_field_from_parent():
     frameworks = Namespace(
         gama=Namespace(name="gama", version="latest", description="flexible automl"),
         gama_old=Namespace(name="gama_20.1.0", version="20.1.0", extends="gama"),
     )
     update_frameworks_with_parent_definitions(frameworks)
     assert frameworks.gama_old.description == "flexible automl"
+
+
+def test_update_frameworks_with_parent_definitions_does_not_overwrite_child_yaml():
+    frameworks = Namespace(
+        gama=Namespace(name="gama", version="latest", description="flexible automl"),
+        gama_old=Namespace(name="gama_20.1", version="20.1", description="old gama", extends="gama"),
+    )
+    update_frameworks_with_parent_definitions(frameworks)
+    assert frameworks.gama_old.description == "old gama"
 
 
 def test_update_frameworks_with_parent_definitions_parent_overwrites_grandparent_yaml():
@@ -119,15 +122,6 @@ def test_update_frameworks_with_parent_definitions_parent_overwrites_grandparent
     )
     update_frameworks_with_parent_definitions(frameworks)
     assert frameworks.gama_oldest.description == "automl"
-
-
-def test_update_frameworks_with_parent_definitions_does_not_overwrite_child_yaml():
-    frameworks = Namespace(
-        gama=Namespace(name="gama", version="latest", description="flexible automl"),
-        gama_old=Namespace(name="gama_20.1", version="20.1", description="old gama", extends="gama"),
-    )
-    update_frameworks_with_parent_definitions(frameworks)
-    assert frameworks.gama_old.description == "old gama"
 
 
 def test_sanitize_and_add_defaults_root_definition_get_module(simple_resource):
@@ -145,7 +139,7 @@ def test_sanitize_and_add_defaults_child_inherits_module(simple_resource):
     assert frameworks.auto_sklearn_old.module == "frameworks.auto_sklearn"
 
 
-def test_sanitize_and_add_defaults_module_not_overwritten(simple_resource):
+def test_sanitize_and_add_defaults_defined_module_not_overwritten(simple_resource):
     frameworks = Namespace(auto_sklearn=Namespace(module="custom_module"))
     sanitize_and_add_defaults(frameworks, simple_resource)
     assert frameworks.auto_sklearn.module == "custom_module"
