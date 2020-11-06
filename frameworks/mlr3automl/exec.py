780701aff8c5fbad75dd4b9e6a7e55e4dcc185f0
a-hanf <alexanderhanf@gmx.de>
2020-11-06 21:34:47 +0100

    save tuning archives

--- frameworks/mlr3automl/exec.py
+++ frameworks/mlr3automl/exec.py
@@ -12,12 +12,10 @@ def run(dataset: Dataset, config: TaskConfig):
     #TODO: use rpy2 instead? not necessary here though as the call is very simple
     log.info("\n**** mlr3automl (R) ****\n")
 
-    is_classification = config.type == 'classification'
-
     here = dir_of(__file__)
 
     with Timer() as training:
-        run_cmd(r"""Rscript --vanilla -e "source('{script}'); run('{train}', '{test}', target.index = {target_index}, '{type}', '{output}', {cores}, time.budget = {time_budget}, seed = {seed})" """.format(
+        run_cmd(r"""Rscript --vanilla -e "source('{script}'); run('{train}', '{test}', target.index = {target_index}, '{type}', '{output}', {cores}, time.budget = {time_budget}, seed = {seed}, name = '{name}')" """.format(
             script=os.path.join(here, 'exec.R'),
             train=dataset.train.path,
             test=dataset.test.path,
@@ -26,7 +24,8 @@ def run(dataset: Dataset, config: TaskConfig):
             output=config.output_predictions_file,
             cores=config.cores,
             time_budget=config.max_runtime_seconds,
-            seed=config.seed
+            seed=config.seed,
+            name=config.name
         ), _live_output_=True)
 
     log.info("Predictions saved to %s", config.output_predictions_file)
