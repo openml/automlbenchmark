e853d8c21b380378aea3c8ef3afe1beba922d877
dev-rinchin <rinchin.damdinov@yandex.ru>
2020-11-23 17:43:34 +0300

    add Zermelo whl to H2O

--- frameworks/H2OAutoML/setup.sh
+++ frameworks/H2OAutoML/setup.sh
@@ -4,7 +4,7 @@ H2O_REPO=${2:-"https://h2o-release.s3.amazonaws.com/h2o"}
 echo "setting up H2O version $VERSION"
 
 HERE=$(dirname "$0")
-. ${HERE}/../shared/setup.sh
+. ${HERE}/../shared/setup.sh ${HERE}/../../
 if [[ -x "$(command -v apt-get)" ]]; then
     SUDO apt-get update
     SUDO apt-get install -y openjdk-8-jdk
@@ -13,6 +13,8 @@ PIP install --no-cache-dir -r ${HERE}/requirements.txt
 
 if  [[ "$VERSION" = "latest" ]]; then
     h2o_package="h2o"
+elif [[ "$VERSION" = "zermelo" ]]; then
+    h2o_package="${H2O_REPO}/rel-zermelo/1/Python/h2o-3.32.0.1-py2.py3-none-any.whl"
 elif [[ "$VERSION" = "zahradnik" ]]; then
     h2o_package="${H2O_REPO}/rel-zahradnik/4/Python/h2o-3.30.0.4-py2.py3-none-any.whl"
 elif [[ "$VERSION" = "yule" ]]; then
