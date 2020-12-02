5e5464fb08571d1a484db506c233a00d5685c4ed
a-hanf <alexanderhanf@gmx.de>
2020-12-02 12:18:45 +0100

    revert some changes

--- run_constant.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo env PATH="$PATH" python runbenchmark.py constantpredictor small 1h10c
-sudo env PATH="$PATH" python runbenchmark.py constantpredictor medium 1h10c
-sudo env PATH="$PATH" python runbenchmark.py constantpredictor large 1h10c
-sudo env PATH="$PATH" python runbenchmark.py constantpredictor regression 1h10c
