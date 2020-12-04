7a33f2fa64426b246e63f8be8ceaaae0730360ad
Seb <sebastien@h2o.ai>
2020-12-04 15:53:10 +0100

    h2o fix for target categories with numerical values (#207)

--- frameworks/H2OAutoML/exec.py
+++ frameworks/H2OAutoML/exec.py
@@ -2,6 +2,7 @@ import contextlib
 import logging
 import os
 import psutil
+import re
 import shutil
 
 import h2o
@@ -194,6 +195,10 @@ def save_predictions(model, test, dataset, config, predictions_file=None, previe
     predictions = y_pred.values
     probabilities = preds.iloc[:, 1:].values
     prob_labels = h2o_preds[0][1:]
+    if all([re.fullmatch(r"p\d+", p) for p in prob_labels]):
+        # for categories represented as numerical values, h2o prefixes the probabilities columns with p
+        # in this case, we let the app setting the labels to avoid mismatch
+        prob_labels = None
     truth = y_truth.values
 
     save_predictions_to_file(dataset=dataset,
