b5b694d52368df9f8085f87bbea5eaec8f6c7b17
PGijsbers <p.gijsbers@tue.nl>
2020-12-16 13:49:43 +0100

    Add prediction loading
    For now we are working with the assumption all tasks are 10-fold CV.

--- amlb/uploads.py
+++ amlb/uploads.py
@@ -15,92 +15,110 @@ from .__version__ import __version__
 log = logging.getLogger(__name__)
 
 
-def _load_predictions(task: OpenMLTask, prediction_files: List[str]) -> pd.DataFrame:
-    results = []
-    for prediction_file in prediction_files:
-        results.append(pd.read_csv(prediction_file, sep=',', header=0))
+def _load_fold(task_folder: str, fold: int, task: OpenMLTask) -> pd.DataFrame:
+    """ Load the predictions and add openml repeat/fold/index information. """
+    prediction_file = os.path.join(task_folder, f"{fold}/predictions.csv")
+    predictions = pd.read_csv(prediction_file, sep=',', header=0)
+
+    train_indices, test_indices = task.get_train_test_split_indices(fold, repeat=0, sample=0)
+    predictions["index"] = test_indices
+    predictions["fold"] = fold
+    predictions["repeat"] = 0
+    return predictions
+
+
+def _load_predictions(task_folder: str) -> pd.DataFrame:
+    """ Loads predictions of all folds for a task with index information required for upload. """
+    metadata = _load_task_data(task_folder)
+    task = openml.tasks.get_task(metadata.openml_task_id)
+    results = [_load_fold(task_folder, fold, task) for fold in range(10)]
     return pd.concat(results)
 
 
-def _load_task_data(task_folder: str) -> Tuple[Namespace, pd.DataFrame]:
-    """ Loads the metadata as a namespace, and predictions as dataframe. """
-    with open(os.path.join(task_folder, '0', 'metadata.json'), 'r') as fh:
+def _load_task_data(task_folder: str, fold: int = 0) -> Namespace:
+    """ Loads the metadata of the given fold of a task as a namespace. """
+    with open(os.path.join(task_folder, str(fold), 'metadata.json'), 'r') as fh:
         metadata = json.load(fh)
     metadata = Namespace.from_dict(metadata)
-    prediction_files = []
-    predictions = _load_predictions(None, prediction_files)
-    return task, metadata, predictions
+    return metadata
 
 
-def _list_completed_folds(task_folder: str) -> Set[str]:
-    completed_folds = set()
-    for fold_dir in os.listdir(task_folder):
-        if "predictions.csv" in os.listdir(os.path.join(task_folder, fold_dir)):
-            completed_folds.add(fold_dir)
-    return completed_folds
-
-
-def _get_flow(metadata: Namespace) -> openml.flows.OpenMLFlow:
-    amlb_flow = openml.flows.OpenMLFlow(
-        name=f"amlb_{metadata.framework}",
-        description=f'{metadata.framework} as set up by the AutoML Benchmark',
-        # todo: use something more thorough like for image names
-        external_version=f'amlb=={__version__},framework=={metadata.framework_version}',
-        # The values below are default values for a flow., the run will record used values.
-        parameters=OrderedDict(
-            time='240',
-            memory='32',
-            cores='8'
-        ),
-        parameters_meta_info=OrderedDict(
-            time=dict(data_type='int', description='time in minutes'),
-            memory=dict(data_type='int', description='memory in gigabytes'),
-            cores=dict(data_type='int', description='number of available cores')
-        ),
-        language='English',
-        # We can use components to describe subflows, e.g. the automl framework with its hyperparameters.
-        # For now we don't.
-        # components=OrderedDict(automl_tool=auto_sklearn_flow),
-    )
-    # If the flow does not yet exist on the server, it is registered.
-    # Otherwise the local version is overwritten the
-    return amlb_flow.publish()
-
-
-def _create_run(metadata, predictions) -> openml.runs.OpenMLRun:
-    pass
-
-
-
-
-
-def _upload_results(task_folder: str) -> openml.runs.OpenMLRun:
-    #
-    # oml_flow = _get_flow(metadata)
-    # oml_run = _create_run(metadata, predictions)
-    #
-    # # load meta-data
-    # # load predictions
-    # return openml.runs.OpenMLRun(
-    #     task_id=task_id, flow_id=flow_id, dataset_id=dataset_id,
-    #     parameter_settings=parameters,
-    #     setup_string=benchmark_command,
-    #     data_content=predictions,
-    #     tags=['study_218']
-    # )
-    pass
-
-
-def process_task_folder(task_folder: str) -> Optional[openml.runs.OpenMLRun]:
-    """ Uploads """
-    completed_folds = _list_completed_folds(task_folder)
-    is_ready_for_upload = (len(completed_folds) == 10)
-    if not is_ready_for_upload:
-        log.warning(
-            "Task %s is missing predictions for folds %s.",
-            task_folder,
-            ', '.join(completed_folds)
-        )
-        return None
-
-    return _upload_results(task_folder)
+# def _list_completed_folds(task_folder: str) -> Set[str]:
+#     completed_folds = set()
+#     for fold_dir in os.listdir(task_folder):
+#         if "predictions.csv" in os.listdir(os.path.join(task_folder, fold_dir)):
+#             completed_folds.add(fold_dir)
+#     return completed_folds
+#
+#
+# def _get_flow(metadata: Namespace) -> openml.flows.OpenMLFlow:
+#     amlb_flow = openml.flows.OpenMLFlow(
+#         name=f"amlb_{metadata.framework}",
+#         description=f'{metadata.framework} as set up by the AutoML Benchmark',
+#         # todo: use something more thorough like for image names
+#         external_version=f'amlb=={__version__},framework=={metadata.framework_version}',
+#         # The values below are default values for a flow., the run will record used values.
+#         parameters=OrderedDict(
+#             time='240',
+#             memory='32',
+#             cores='8'
+#         ),
+#         parameters_meta_info=OrderedDict(
+#             time=dict(data_type='int', description='time in minutes'),
+#             memory=dict(data_type='int', description='memory in gigabytes'),
+#             cores=dict(data_type='int', description='number of available cores')
+#         ),
+#         language='English',
+#         # We can use components to describe subflows, e.g. the automl framework with its hyperparameters.
+#         # For now we don't.
+#         # components=OrderedDict(automl_tool=auto_sklearn_flow),
+#     )
+#     # If the flow does not yet exist on the server, it is registered.
+#     # Otherwise the local version is overwritten the
+#     return amlb_flow.publish()
+#
+#
+# def _create_run(task_folder: str) -> openml.runs.OpenMLRun:
+#     predictions = _load_predictions(task_folder)
+#     metadata = _load_task_data(task_folder)
+#
+#  # prediction = format_prediction(
+#  #        task=task,
+#  #        repeat=repeat,
+#  #        fold=fold,
+#  #        index=index,
+#  #        prediction=class_map[yp],
+#  #        truth=y,
+#  #        proba={c: pb for (c, pb) in zip(task.class_labels, proba)},
+#  #    )
+#
+# def _upload_results(task_folder: str) -> openml.runs.OpenMLRun:
+#     #
+#     # oml_flow = _get_flow(metadata)
+#     # oml_run = _create_run(metadata, predictions)
+#     #
+#     # # load meta-data
+#     # # load predictions
+#     # return openml.runs.OpenMLRun(
+#     #     task_id=task_id, flow_id=flow_id, dataset_id=dataset_id,
+#     #     parameter_settings=parameters,
+#     #     setup_string=benchmark_command,
+#     #     data_content=predictions,
+#     #     tags=['study_218']
+#     # )
+#     pass
+#
+#
+# def process_task_folder(task_folder: str) -> Optional[openml.runs.OpenMLRun]:
+#     """ Uploads """
+#     completed_folds = _list_completed_folds(task_folder)
+#     is_ready_for_upload = (len(completed_folds) == 10)
+#     if not is_ready_for_upload:
+#         log.warning(
+#             "Task %s is missing predictions for folds %s.",
+#             task_folder,
+#             ', '.join(completed_folds)
+#         )
+#         return None
+#
+#     return _upload_results(task_folder)
--- tests/unit/amlb/uploads/test_file_loading.py
+++ tests/unit/amlb/uploads/test_file_loading.py
@@ -1,9 +1,9 @@
 import os
 
+import openml
 import pandas as pd
-import pytest
 
-from amlb.uploads import _load_predictions
+from amlb.uploads import _load_predictions, _load_fold
 
 here = os.path.realpath(os.path.dirname(__file__))
 res = os.path.join(here, 'resources')
@@ -11,14 +11,27 @@ res = os.path.join(here, 'resources')
 iris_complete = f"{res}/iris/"
 
 
-def test__load_predictions_loads_all_files():
-    prediction_files = [os.path.join(iris_complete, f"{i}/predictions.csv") for i in range(10)]
-    task = None
-    predictions = _load_predictions(None, prediction_files)
+def test__load_fold_loads_prediction_file():
+    iris_task = openml.tasks.get_task(59)
+    predictions = _load_fold(iris_complete, 0, iris_task)
     assert isinstance(predictions, pd.DataFrame)
-    assert predictions.shape == (150, 5)
-    assert list(predictions.columns) == ["iris-setosa", "iris-versicolor", "iris-virginica", "predictions", "truth"]
+    assert predictions.shape == (15, 8)
+    assert set(predictions.columns) == {"repeat", "fold", "index", "iris-setosa", "iris-versicolor", "iris-virginica", "predictions", "truth"}
 
 
-def test__load_predictions_reorders():
-    pass
+def test__load_fold_attaches_correct_index_information():
+    iris_task = openml.tasks.get_task(59)
+    predictions = _load_fold(iris_complete, 0, iris_task)
+    assert list(predictions["index"]) == [43, 14, 37, 23, 10, 99, 87, 97, 62, 92, 119, 111, 144, 116, 125]
+    assert all(predictions["fold"] == 0)
+    predictions = _load_fold(iris_complete, 1, iris_task)
+    assert list(predictions["index"]) == [49, 15, 47,  0, 44, 76, 89, 58, 54, 53, 149, 114, 112, 115, 139]
+    assert all(predictions["fold"] == 1)
+
+
+def test__load_predictions_loads_all_files():
+    predictions = _load_predictions(iris_complete)
+    assert isinstance(predictions, pd.DataFrame)
+    assert predictions.shape == (150, 8)
+    assert set(predictions.columns) == {"repeat", "fold", "index", "iris-setosa", "iris-versicolor", "iris-virginica", "predictions", "truth"}
+    assert set(predictions["fold"]) == set(range(10))
