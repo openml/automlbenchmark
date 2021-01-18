15782f9a22e6682b9c5d73de78e99de48613b117
Sebastien Poirier <sebastien@h2o.ai>
2021-01-18 18:01:30 +0100

    fixing outdated examples

--- examples/custom/extensions/Stacking/exec.py
+++ examples/custom/extensions/Stacking/exec.py
@@ -14,13 +14,14 @@ from sklearn.ensemble import StackingClassifier, StackingRegressor
 from sklearn.linear_model import LinearRegression, LogisticRegression, SGDClassifier, SGDRegressor
 from sklearn.svm import LinearSVC, LinearSVR
 
-from frameworks.shared.callee import call_run, result, Timer
+from frameworks.shared.callee import call_run, result, save_metadata, utils
 
 log = logging.getLogger(os.path.basename(__file__))
 
 
 def run(dataset, config):
-    log.info("\n**** Stacking Ensemble (sklearn %s) ****\n", sklearn.__version__)
+    log.info(f"\n**** Stacking Ensemble [sklearn v{sklearn.__version__}] ****\n")
+    save_metadata(config, version=sklearn.__version__)
 
     is_classification = config.type == 'classification'
 
@@ -62,7 +63,7 @@ def run(dataset, config):
             **training_params
         )
 
-    with Timer() as training:
+    with utils.Timer() as training:
         estimator.fit(X_train, y_train)
 
     predictions = estimator.predict(X_test)
