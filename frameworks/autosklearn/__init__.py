3a88d2dec803194e2665b0f349fa597d525087c9
PGijsbers <p.gijsbers@tue.nl>
2021-01-20 17:39:54 +0100

    change file to test workflow

--- frameworks/autosklearn/__init__.py
+++ frameworks/autosklearn/__init__.py
@@ -3,7 +3,7 @@ from amlb.data import Dataset
 from amlb.resources import config as rconfig
 from amlb.utils import call_script_in_same_dir
 
-
+# changed file
 def setup(*args, **kwargs):
     call_script_in_same_dir(__file__, "setup.sh", rconfig().root_dir, *args, **kwargs)
 
