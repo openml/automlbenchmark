51259071168d474f4ead07be30c10d949f05f650
BigBigMiao <BigBigMiao@github.com>
2021-03-16 13:23:20 -0700

    update based on comment

--- frameworks/MLNet/setup.sh
+++ frameworks/MLNet/setup.sh
@@ -2,15 +2,15 @@
 HERE=$(dirname "$0")
 MLNET='mlnet'
 VERSION="$1"
-DOTNET_INSTALL_DIR="$HERE/.dotnet"
+DOTNET_INSTALL_DIR="$HERE/lib"
 MLNET="$DOTNET_INSTALL_DIR/mlnet"
 DOTNET="$DOTNET_INSTALL_DIR/dotnet"
-SOURCE="$2"
+SOURCE="/mnt/c/Users/xiaoyuz/source/repos/machinelearning-tools/artifacts/packages/Release/Shipping/"
 # install mlnet if necessary
 if [[ ! -x "$MLNET" ]]; then
     if [[ ! -x "$DOTNET" ]]; then
-        wget https://dot.net/v1/dotnet-install.sh   
-        ./dotnet-install.sh -c Current --install-dir "$DOTNET_INSTALL_DIR" -Channel 3.1
+        wget -P "$DOTNET_INSTALL_DIR" https://dot.net/v1/dotnet-install.sh   
+        "$DOTNET_INSTALL_DIR/dotnet-install.sh" -c Current --install-dir "$DOTNET_INSTALL_DIR" -Channel 3.1
     fi
 
     $DOTNET --version
