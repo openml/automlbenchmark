83c4da28cc506f23fe17fb7154ff103fb8d6e7ab
LittleLittleCloud <bigmiao.zhang@gmail.com>
2021-03-27 14:39:33 -0700

    fix based on comments

--- frameworks/MLNet/exec.py
+++ frameworks/MLNet/exec.py
@@ -1,5 +1,6 @@
 import logging
-import math
+import tempfile
+import psutil
 import os
 import pandas as pd
 from amlb.benchmark import TaskConfig
@@ -25,14 +26,15 @@ def run(dataset: Dataset, config: TaskConfig):
     os.environ['MODELBUILDER_AUTOML'] = 'NNI'
     os.environ['DOTNET_ROOT'] = DOTNET_INSTALL_DIR
     os.environ['MLNetCLIEnablePredict'] = 'True'
+    threads_count_per_core = psutil.cpu_count() / psutil.cpu_count(logical=False)
+    os.environ['MLNET_MAX_THREAD'] = str(config.cores * threads_count_per_core)
     mlnet = os.path.join(DOTNET_INSTALL_DIR, 'mlnet')
-    temp_output_folder = output_subdir('', config=config)
     train_time_in_seconds = config.max_runtime_seconds
-    log_path = os.path.join(temp_output_folder, 'log.txt')
     sub_command = config.type
-    train_dataset = os.path.join(config.output_dir, config.name, f'train_{config.fold}.csv')
-    test_dataset = os.path.join(config.output_dir, config.name, f'test_{config.fold}.csv')
-    if not os.path.exists(train_dataset) or not os.path.exists(test_dataset):
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        train_dataset = os.path.join(tmpdir, f'train.csv')
+        test_dataset = os.path.join(tmpdir, f'test.csv')
         column_num = dataset.train.X.shape[1]
         columns=[f'column_{i}' for i in range(column_num)]
         train_df = pd.DataFrame(dataset.train.X, columns=columns)
@@ -44,53 +46,55 @@ def run(dataset: Dataset, config: TaskConfig):
         train_df.to_csv(train_dataset, index=False, header=True)
         log.info(f'saving test to {test_dataset}')
         test_df.to_csv(test_dataset, index=False, header=True)
-
-    with Timer() as training:
+        temp_output_folder = os.path.join(tmpdir, str(config.fold))
+        log_path = os.path.join(temp_output_folder, 'log.txt')
         cmd =   f"{mlnet} {sub_command}"\
                 f" --dataset {train_dataset} --test-dataset {test_dataset} --train-time {train_time_in_seconds}"\
                 f" --label-col label --output {os.path.dirname(temp_output_folder)} --name {config.fold}"\
                 f" --verbosity q --log-file-path {log_path}"
-        run_cmd(cmd)
+        
+        with Timer() as training:
+            run_cmd(cmd)
 
         train_result_json = os.path.join(temp_output_folder, '{}.mbconfig'.format(config.fold))
         if not os.path.exists(train_result_json):
             raise NoResultError("MLNet failed producing any prediction.")
         
-    with open(train_result_json, 'r') as f:
-        json_str = f.read()
-        mb_config = json.loads(json_str)
-        model_path = mb_config['Artifact']['MLNetModelPath']
-        output_prediction_txt = config.output_predictions_file.replace('.csv', '.txt')
-        models_count = len(mb_config['RunHistory']['Trials'])
+        with open(train_result_json, 'r') as f:
+            json_str = f.read()
+            mb_config = json.loads(json_str)
+            model_path = mb_config['Artifact']['MLNetModelPath']
+            output_prediction_txt = os.path.join(tmpdir, "prediction.txt")
+            models_count = len(mb_config['RunHistory']['Trials'])
 
-        # predict
-        predict_cmd =   f"{mlnet} predict --task-type {config.type}" \
-                        f" --model {model_path} --dataset {test_dataset} > {output_prediction_txt}"
-        with Timer() as prediction:
-            run_cmd(predict_cmd)
+            # predict
+            predict_cmd =   f"{mlnet} predict --task-type {config.type}" \
+                            f" --model {model_path} --dataset {test_dataset} > {output_prediction_txt}"
+            with Timer() as prediction:
+                run_cmd(predict_cmd)
 
-        if config.type == 'classification':
-            prediction_df = pd.read_csv(output_prediction_txt, dtype={'PredictedLabel':'object'})
-            save_predictions(
-                dataset=dataset,
-                output_file=config.output_predictions_file,
-                predictions=prediction_df['PredictedLabel'].values,
-                truth=dataset.test.y,
-                probabilities=prediction_df.values[:,:-1],
-                probabilities_labels=list(prediction_df.columns.values[:-1]),
-            )
+            if config.type == 'classification':
+                prediction_df = pd.read_csv(output_prediction_txt, dtype={'PredictedLabel':'object'})
+                save_predictions(
+                    dataset=dataset,
+                    output_file=config.output_predictions_file,
+                    predictions=prediction_df['PredictedLabel'].values,
+                    truth=dataset.test.y,
+                    probabilities=prediction_df.values[:,:-1],
+                    probabilities_labels=list(prediction_df.columns.values[:-1]),
+                )
         
-        if config.type == 'regression':
-            prediction_df = pd.read_csv(output_prediction_txt)
-            save_predictions(
-                dataset=dataset,
-                output_file=config.output_predictions_file,
-                predictions=prediction_df['Score'].values,
-                truth=dataset.test.y,
-            )
+            if config.type == 'regression':
+                prediction_df = pd.read_csv(output_prediction_txt)
+                save_predictions(
+                    dataset=dataset,
+                    output_file=config.output_predictions_file,
+                    predictions=prediction_df['Score'].values,
+                    truth=dataset.test.y,
+                )
 
-        return dict(
-                models_count = models_count,
-                training_duration=training.duration,
-                predict_duration=prediction.duration,
-            )
+            return dict(
+                    models_count = models_count,
+                    training_duration=training.duration,
+                    predict_duration=prediction.duration,
+                )
