7f44be8e41ad4c041fca4b5916b34fdc500346d1
PGijsbers <p.gijsbers@tue.nl>
2021-06-29 13:42:50 +0200

    Add stable and latest downloads to AutoWEKA

--- frameworks/AutoWEKA/setup.sh
+++ frameworks/AutoWEKA/setup.sh
@@ -7,12 +7,19 @@ if [[ -x "$(command -v apt-get)" ]]; then
     SUDO apt-get install -y wget unzip openjdk-8-jdk
 fi
 
-AUTOWEKA_ARCHIVE="autoweka-${VERSION}.zip"
+if [[ "$VERSION" == "latest" || "$VERSION" == "stable" ]]; then
+    URL="https://raw.githubusercontent.com/automl/autoweka/master"
+    AUTOWEKA_ARCHIVE="autoweka-latest.zip"
+else
+    URL="http://www.cs.ubc.ca/labs/beta/Projects/autoweka"
+    AUTOWEKA_ARCHIVE="autoweka-${VERSION}.zip"
+fi
+
 DOWNLOAD_DIR="$HERE/lib"
 TARGET_DIR="$DOWNLOAD_DIR/autoweka"
 rm -Rf ${TARGET_DIR}
 
-wget http://www.cs.ubc.ca/labs/beta/Projects/autoweka/$AUTOWEKA_ARCHIVE -P $DOWNLOAD_DIR
-unzip $DOWNLOAD_DIR/$AUTOWEKA_ARCHIVE -d $TARGET_DIR
+wget "$URL/$AUTOWEKA_ARCHIVE" -P $DOWNLOAD_DIR
+unzip "$DOWNLOAD_DIR/$AUTOWEKA_ARCHIVE" -d $TARGET_DIR
 
 find $HERE/lib/autoweka*.zip | sed -e 's/.*\/autoweka-\(.*\)\.zip/\1/' >> "${HERE}/.installed"
