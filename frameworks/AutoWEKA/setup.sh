ca08cbbb45f45f797557f1e485a5d3f0e273c13d
PGijsbers <p.gijsbers@tue.nl>
2021-07-01 13:10:05 +0200

    Download WEKA for newer versions of AutoWEKA
    The weka.jar file is no longer included with AutoWEKA for the latest
    releases, and thus needs to be downloaded (and extracted) separately.

--- frameworks/AutoWEKA/setup.sh
+++ frameworks/AutoWEKA/setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 HERE=$(dirname "$0")
 VERSION=${1:-"stable"}
+WEKA=${2:-"https://prdownloads.sourceforge.net/weka/weka-3-8-5-azul-zulu-linux.zip"}
+
 . ${HERE}/../shared/setup.sh "${HERE}"
 if [[ -x "$(command -v apt-get)" ]]; then
     SUDO apt-get update
@@ -17,9 +19,18 @@ fi
 
 DOWNLOAD_DIR="$HERE/lib"
 TARGET_DIR="$DOWNLOAD_DIR/autoweka"
-rm -Rf ${TARGET_DIR}
+rm -Rf "${TARGET_DIR}"
 
-wget "$URL/$AUTOWEKA_ARCHIVE" -P $DOWNLOAD_DIR
-unzip "$DOWNLOAD_DIR/$AUTOWEKA_ARCHIVE" -d $TARGET_DIR
+wget "$URL/$AUTOWEKA_ARCHIVE" -P "$DOWNLOAD_DIR"
+unzip "$DOWNLOAD_DIR/$AUTOWEKA_ARCHIVE" -d "$TARGET_DIR"
+
+if [[ "$VERSION" == "latest" || "$VERSION" == "stable" ]]; then
+  # At some point after 2.6 the weka.jar is no longer included in the autoweka archive
+  wget "$WEKA" -O "$DOWNLOAD_DIR/weka.zip"
+  unzip "$DOWNLOAD_DIR/weka.zip" -d "$DOWNLOAD_DIR"
+  # The weka.zip archive contains a folder named weka-VERSION.
+  # By renaming it we can make the exec.py agnostic of the specific name.
+  (cd "$DOWNLOAD_DIR" && find . -maxdepth 1 -type d -name "weka*" -exec mv {} weka \;)
+fi
 
 find $HERE/lib/autoweka*.zip | sed -e 's/.*\/autoweka-\(.*\)\.zip/\1/' >> "${HERE}/.installed"
