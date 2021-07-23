bacd98fef5225000b6672802cdc579c0de129042
Sebastien Poirier <sebastien@h2o.ai>
2021-07-23 19:01:32 +0200

    improved comment

--- frameworks/shared/serialization.py
+++ frameworks/shared/serialization.py
@@ -38,9 +38,9 @@ __series__ = '_series_'
 def serialize_data(data, path):
     np, pd = _import_data_libraries()
     if pd and isinstance(data, (pd.DataFrame, pd.Series)):
-        # pandas has this nasty habit of inferring values when data are loaded from file,
-        # for example, 'True', 'False' are converted automatically to booleans, even for column names…
         if isinstance(data, pd.DataFrame):
+            # pandas has this habit of inferring value types when data are loaded from file,
+            # for example, 'true' and 'false' are converted automatically to booleans, even for column names…
             data.rename(str, axis='columns', inplace=True)
         ser = ser_config.pandas_serializer
         if ser == 'pickle':
