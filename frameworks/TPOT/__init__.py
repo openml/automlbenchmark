f4c1eda58c7c91860781167392393e8de4d15205
Seb <sebastien@h2o.ai>
2021-07-29 12:30:47 +0200

    TPOT doesn't need missing values imputation anymore (#372)

--- frameworks/TPOT/__init__.py
+++ frameworks/TPOT/__init__.py
@@ -11,7 +11,7 @@ def setup(*args, **kwargs):
 def run(dataset: Dataset, config: TaskConfig):
     from frameworks.shared.caller import run_in_venv
 
-    X_train, X_test = impute_array(dataset.train.X_enc, dataset.test.X_enc)
+    X_train, X_test = dataset.train.X_enc, dataset.test.X_enc
     y_train, y_test = dataset.train.y_enc, dataset.test.y_enc
     data = dict(
         train=dict(
