4d9cc42e7cb3ab90b6cfb071ff138db4f3a71cf3
PGijsbers <p.gijsbers@tue.nl>
2021-09-03 14:57:11 +0200

    Fix MLNet on AWS (#385)
    Configure variables needed for non-default installation without `HOME` directory.
    Fix an issue that would cause the version information to be parsed wrong.

--- frameworks/MLNet/setup.sh
+++ frameworks/MLNet/setup.sh
@@ -2,32 +2,32 @@
 HERE=$(dirname "$0")
 MLNET='mlnet'
 VERSION=${1:-"latest"}
+
 DOTNET_INSTALL_DIR="$HERE/lib"
 MLNET="$DOTNET_INSTALL_DIR/mlnet"
 DOTNET="$DOTNET_INSTALL_DIR/dotnet"
 SOURCE="https://mlnetcli.blob.core.windows.net/mlnetcli/index.json"
+
+export DOTNET_CLI_HOME="$DOTNET_INSTALL_DIR"
 
 # if version eq latest, set Version to empty string so it will install the latest version.
 if [[ "$VERSION" == "latest" ]]; then
     VERSION=""
 fi
 
-rm -rf DOTNET_INSTALL_DIR
+rm -rf "$DOTNET_INSTALL_DIR"
 # install mlnet if necessary
 if [[ ! -x "$MLNET" ]]; then
     if [[ ! -x "$DOTNET" ]]; then
         wget -P "$DOTNET_INSTALL_DIR" https://dot.net/v1/dotnet-install.sh
         chmod +x "$DOTNET_INSTALL_DIR/dotnet-install.sh"
-        "$DOTNET_INSTALL_DIR/dotnet-install.sh" -c Current --install-dir "$DOTNET_INSTALL_DIR" -Channel 3.1
+        "$DOTNET_INSTALL_DIR/dotnet-install.sh" -c Current --install-dir "$DOTNET_INSTALL_DIR" -Channel 3.1 --verbose
     fi
-
-    $DOTNET --version
     $DOTNET tool install mlnet --add-source "$SOURCE" --version "$VERSION" --tool-path "$DOTNET_INSTALL_DIR"
 else
-$DOTNET tool update mlnet --add-source "$SOURCE" --version "$VERSION" --tool-path "$DOTNET_INSTALL_DIR"
+  $DOTNET tool update mlnet --add-source "$SOURCE" --version "$VERSION" --tool-path "$DOTNET_INSTALL_DIR"
 fi
 
 export DOTNET_ROOT="$DOTNET_INSTALL_DIR"
-export MLNET_CLI_HOME="$DOTNET_INSTALL_DIR"
 
-$MLNET --version | sed -e "s/\(.?*\)\+.*/\1/" >> "${HERE}/.setup/installed"
+$MLNET --version | grep + | sed -e "s/\(.?*\)+.*/\1/" >> "${HERE}/.setup/installed"
