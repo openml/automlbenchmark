1a399944683d31c5303bf52be96bf78006436623
eddiebergman <eddiebergmanhs@gmail.com>
2021-10-03 21:24:55 +0200

    Updated hyper opt not to use PyPi and accept shas

--- frameworks/hyperoptsklearn/setup.sh
+++ frameworks/hyperoptsklearn/setup.sh
@@ -3,21 +3,27 @@ HERE=$(dirname "$0")
 VERSION=${1:-"stable"}
 REPO=${2:-"https://github.com/hyperopt/hyperopt-sklearn.git"}
 PKG=${3:-"hyperopt-sklearn"}
-if [[ "$VERSION" == "latest" ]]; then
+
+if [[ "$VERSION" == "latest" || "$VERSION" == "stable" ]]; then
     VERSION="master"
 fi
 
 . ${HERE}/../shared/setup.sh ${HERE} true
 
-if [[ "$VERSION" == "stable" ]]; then
-    PIP install --no-cache-dir -U ${PKG}
-elif [[ "$VERSION" =~ ^[0-9] ]]; then
-    PIP install --no-cache-dir -U ${PKG}==${VERSION}
+if [[ "$VERSION" =~ ^[0-9] ]]; then
+    pip install git+https://github.com/hyperopt/hyperopt-sklearn@${VERSION}
 else
 #    PIP install --no-cache-dir -e git+${REPO}@${VERSION}#egg=${PKG}
     LIB=$(echo ${PKG} | sed "s/\[.*\]//")
     TARGET_DIR="${HERE}/lib/${LIB}"
     rm -Rf ${TARGET_DIR}
-    git clone --depth 1 --single-branch --branch ${VERSION} --recurse-submodules ${REPO} ${TARGET_DIR}
+    if [[ "$VERSION" =~ ^# ]]; then
+        git clone --recurse-submodules --shallow-submodules ${REPO} ${TARGET_DIR}
+        cd ${TARGET_DIR}
+        git checkout "${VERSION:1}"
+        cd ${HERE}
+    else
+        git clone --depth 1 --single-branch --branch ${VERSION} --recurse-submodules ${REPO} ${TARGET_DIR}
+    fi
     PIP install -U -e ${HERE}/lib/${PKG}
 fi
