27ac90fe1b6f323547fad9fbc465cfa9b42fd207
Weisu Yin <weisuyin96@gmail.com>
2021-11-18 00:19:20 +0000

    s3 functionality

--- amlb/datasets/fileutils.py
+++ amlb/datasets/fileutils.py
@@ -11,7 +11,22 @@ from ..utils import touch
 
 log = logging.getLogger(__name__)
 
-SUPPORTED_SCHEMES = ("http", "https")
+SUPPORTED_SCHEMES = ("http", "https", "s3")
+
+
+def s3_path_to_bucket_prefix(s3_path):
+    s3_path_cleaned = s3_path.split('://', 1)[1]
+    bucket, prefix = s3_path_cleaned.split('/', 1)
+
+    return bucket, prefix
+
+
+def is_s3_url(path):
+    if type(path) != str:
+        return False
+    if (path[:2] == 's3') and ('://' in path[:6]):
+        return True
+    return False
 
 
 def is_valid_url(url):
@@ -21,21 +36,45 @@ def is_valid_url(url):
 def url_exists(url):
     if not is_valid_url(url):
         return False
-    head_req = Request(url, method='HEAD')
-    try:
-        with urlopen(head_req) as test:
-            return test.status == 200
-    except URLError as e:
-        log.error(f"Cannot access url %s: %s", url, e)
-        return False
+    if not is_s3_url(url):
+        head_req = Request(url, method='HEAD')
+        try:
+            with urlopen(head_req) as test:
+                return test.status == 200
+        except URLError as e:
+            log.error(f"Cannot access url %s: %s", url, e)
+            return False
+    else:
+        import boto3
+        from botocore.errorfactory import ClientError
+        s3 = boto3.client('s3')
+        bucket, key = s3_path_to_bucket_prefix(url)
+        try:
+            s3.head_object(Bucket=bucket, Key=key)
+            return True
+        except ClientError as e:
+            log.error(f"Cannot access url %s: %s", url, e)
+            return False
 
 
 def download_file(url, dest_path):
     touch(dest_path)
     # urlretrieve(url, filename=dest_path)
-    with urlopen(url) as resp, open(dest_path, 'wb') as dest:
-        shutil.copyfileobj(resp, dest)
-
+    if not is_s3_url(url):
+        with urlopen(url) as resp, open(dest_path, 'wb') as dest:
+            shutil.copyfileobj(resp, dest)
+    else:
+        import boto3
+        from botocore.errorfactory import ClientError
+        s3 = boto3.resource('s3')
+        bucket, key = s3_path_to_bucket_prefix(url)
+        try:
+            s3.Bucket(bucket).download_file(key, dest_path)
+        except ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                log.error("The object does not exist.")
+            else:
+                raise
 
 def is_archive(path):
     return zipfile.is_zipfile(path) or tarfile.is_tarfile(path)
@@ -52,4 +91,3 @@ def unarchive_file(path, dest_folder=None):
         with tarfile.open(path) as tf:
             tf.extractall(path=dest_folder)
     return dest
-
