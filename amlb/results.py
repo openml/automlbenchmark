fixes #432 add precision to runtimes in results.csv

amlb/results.py
@@ -169,7 +169,7 @@ class Scoreboard:
         for col in nanable_int_cols:
             df[col] = df[col].astype(np.object).map(int_print).astype(np.str)
         for col in low_precision_float_cols:
-            df[col] = df[col].astype(np.float).map(partial(num_print, "{:.1f}".format)).astype(np.float)
+            df[col] = df[col].astype(np.float).map(partial(num_print, "{:.3g}".format)).astype(np.float)
         for col in high_precision_float_cols:
             df[col] = df[col].map(partial(num_print, "{:.6g}".format)).astype(np.float)
 