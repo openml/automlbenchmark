4ac4bce178783d16be7a8b88d9a17878d5656cf5
PGijsbers <p.gijsbers@tue.nl>
2021-12-15 14:43:25 +0200

    Make iterative fit default, parameterize execution

--- frameworks/RandomForest/exec.py
+++ frameworks/RandomForest/exec.py
@@ -18,6 +18,12 @@ from frameworks.shared.utils import Timer
 log = logging.getLogger(os.path.basename(__file__))
 
 
+def extrapolate_with_worst_case(values: List[float], n: int = 5) -> float:
+    """ Extrapolate the next value for `values`, based on the last `n` samples. """
+    n = min(len(values), n)
+    return values[-1] + max(v_next - v_prev for v_prev, v_next in zip(values[-n:], values[-n + 1:]))
+
+
 def run(dataset, config):
     log.info(f"\n**** Random Forest [sklearn v{sklearn.__version__}] ****\n")
 
@@ -28,10 +34,17 @@ def run(dataset, config):
     X_train, X_test = dataset.train.X, dataset.test.X
     y_train, y_test = dataset.train.y, dataset.test.y
 
-    training_params = {k: v for k, v in config.framework_params.items() if not k.startswith('_')}
+    training_params = {
+        k: v for k, v in config.framework_params.items()
+        if not (k.startswith('_') or k == "n_estimators")
+    }
     n_jobs = config.framework_params.get('_n_jobs', config.cores)  # useful to disable multicore, regardless of the dataset config
-    iterative_fit = config.framework_params.get('warm_start', False)
-    n_trees = config.framework_params.get('n_estimators', 10)
+    step_size = config.framework_params.get('step_size', 10)
+    final_forest_size = config.framework_params.get('n_estimators', 2000)
+
+    # Default margins are conservative, because robustness is paramount for a baseline.
+    time_margin = config.framework_params.get('_time_margin', 0.9)
+    memory_margin = config.framework_params.get('_memory_margin', 0.9)
 
     log.info("Running RandomForest with a maximum time of {}s on {} cores.".format(config.max_runtime_seconds, n_jobs))
     log.warning("We completely ignore the advice to optimize towards metric: {}.".format(config.metric))
@@ -39,21 +52,13 @@ def run(dataset, config):
     estimator = RandomForestClassifier if is_classification else RandomForestRegressor
     rf = estimator(n_jobs=n_jobs,
                    random_state=config.seed,
+                   n_estimators=step_size,
+                   warm_start=True,
                    **training_params)
 
-    training_times = []
-    memory_usage = []
-    time_margin = 0.9
-    memory_margin = 0.9
-
-    def extrapolate_with_worst_case(values: List[float], n: int = 5) -> float:
-        """ Extrapolate the next value for `values`, based on the last `n` samples. """
-        n = min(len(values), n)
-        return values[-1] + max(v_next - v_prev for v_prev, v_next in zip(values[-n:], values[-n+1:]))
-
     with Timer() as training:
-        training_times.append(training.duration)
-        memory_usage.append(this_process.memory_info()[0] / (2**20))
+        training_times = [training.duration]
+        memory_usage = [this_process.memory_info()[0] / (2**20)]
 
         while True:
             rf.fit(X_train, y_train)
@@ -64,7 +69,8 @@ def run(dataset, config):
 
             will_run_out_of_memory = extrapolate_with_worst_case(memory_usage) >= config.max_mem_size_mb * memory_margin
             will_run_out_of_time = extrapolate_with_worst_case(training_times) >= config.max_runtime_seconds * time_margin
-            if not iterative_fit:
+            if rf.n_estimators == final_forest_size:
+                log.info("Stop training because desired forest size has been reached.")
                 break
             elif will_run_out_of_time:
                 log.info("Stop training because it expects to exceed its time budget.")
@@ -74,7 +80,7 @@ def run(dataset, config):
                 break
             else:
                 # https://stackoverflow.com/questions/42757892/how-to-use-warm-start/42763502
-                rf.n_estimators += n_trees
+                rf.n_estimators += step_size
 
     with Timer() as predict:
         predictions = rf.predict(X_test)
