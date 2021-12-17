47e7b32f2e2043528b6f967fb0feb9aa1bfb260b
eddiebergman <eddiebergmanhs@gmail.com>
2021-12-17 17:20:15 +0100

    Respect tmpdir

--- frameworks/shared/caller.py
+++ frameworks/shared/caller.py
@@ -82,7 +82,8 @@ def run_in_venv(caller_file, script_file: str, *args,
                 input_data: Union[dict, ns], dataset: Dataset, config: TaskConfig,
                 options: Union[None, dict, ns] = None,
                 process_results=None,
-                python_exec=None):
+                python_exec=None,
+                retain_tmp_env: bool = True):
     here = dir_of(caller_file)
     if python_exec is None:  # use local virtual env by default
         python_exec = venv_python_exec(here)
@@ -92,6 +93,12 @@ def run_in_venv(caller_file, script_file: str, *args,
     options = ns.from_dict(options) if options else ns()
     ser_config = options['serialization']
     env = options['env'] or ns()
+
+    # To honour any arguments about tmpdir, we copy over
+    # the TMP, TEMP and TMPDIR environment variables
+    if retain_tmp_env:
+        for env_var in ["TMP", "TEMP", "TMPDIR"]:
+            env[env_var] = os.environ.get(env_var)
 
     with TemporaryDirectory() as tmpdir:
 
