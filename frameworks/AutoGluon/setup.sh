0b2a112602eca6e997434c2f57c2583e6b5e8253
Innixma <neerick@amazon.com>
2022-03-22 17:40:22 -0700

    Update to support AutoGluon v0.4

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -9,16 +9,17 @@ fi
 
 # creating local venv
 . ${HERE}/../shared/setup.sh ${HERE} true
-#if [[ -x "$(command -v apt-get)" ]]; then
-#    SUDO apt-get install -y libomp-dev
+
+# Below fixes seg fault on MacOS due to bug in libomp: https://github.com/awslabs/autogluon/issues/1442
 if [[ -x "$(command -v brew)" ]]; then
-    brew install libomp
+    wget https://raw.githubusercontent.com/Homebrew/homebrew-core/fb8323f2b170bd4ae97e1bac9bf3e2983af3fdb0/Formula/libomp.rb
+    brew install libomp.rb
+    rm libomp.rb
 fi
 
 PIP install --upgrade pip
 PIP install --upgrade setuptools wheel
-PIP install "mxnet<2.0.0"
-PIP install "scikit-learn-intelex<2021.3"
+PIP install "scikit-learn-intelex<2021.6"
 
 if [[ "$VERSION" == "stable" ]]; then
     PIP install --no-cache-dir -U ${PKG}
@@ -29,11 +30,15 @@ else
     rm -Rf ${TARGET_DIR}
     git clone --depth 1 --single-branch --branch ${VERSION} --recurse-submodules ${REPO} ${TARGET_DIR}
     cd ${TARGET_DIR}
-    PIP install -e core/
+    # Note: Normally we would just call `./full_install.sh` but because `pip` and `PIP` are not the same,
+    # the script does not work here. Therefore, we have to explicitly install each submodule below.
+    # This has the downside that source installs of old versions of the package might not follow the below steps.
+    # It is recommended to instead use pip install of older AG versions to ensure it works correctly.
+    # https://github.com/awslabs/autogluon/blob/master/full_install.sh
+    PIP install -e common/
     PIP install -e features/
+    PIP install -e core/[all]
     PIP install -e tabular/[all]
-    PIP install -e mxnet/
-    PIP install -e extra/
     PIP install -e text/
     PIP install -e vision/
     PIP install -e autogluon/
