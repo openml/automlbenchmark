6e5d3c46cf1f54f7f4c24f2935bf30f2f2943aba
nicl-nno <nicl.nno@gmail.com>
2022-09-07 00:30:32 +0300

    FEDOT added

---
+++ frameworks/FEDOT/setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+HERE=$(dirname "$0")
+VERSION=${1:-"stable"}
+REPO=${2:-"https://github.com/aimclub/FEDOT.git"}
+PKG=${3:-"fedot"}
+if [[ "$VERSION" == "latest" ]]; then
+    VERSION="master"
+fi
+
+# creating local venv
+. ${HERE}/../shared/setup.sh ${HERE} true
+
+RAWREPO=$(echo ${REPO} | sed "s/github\.com/raw\.githubusercontent\.com/")
+if [[ "$VERSION" == "stable" ]]; then
+    PIP install --no-cache-dir -U ${PKG}
+elif [[ "$VERSION" =~ ^[0-9] ]]; then
+    PIP install --no-cache-dir -U ${PKG}==${VERSION}
+else
+    TARGET_DIR="${HERE}/lib/${PKG}"
+    rm -Rf ${TARGET_DIR}
+    git clone --depth 1 --single-branch --branch ${VERSION} --recurse-submodules ${REPO} ${TARGET_DIR}
+    PIP install -U -e ${HERE}/lib/${PKG}
+fi
