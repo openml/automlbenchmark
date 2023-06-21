41a10c0521dab92ccc8d73c8fd7002db4eba34cb
PGijsbers <p.gijsbers@tue.nl>
2023-06-21 17:08:22 +0200

    Undo markers for hyperopt: integration is broken and disrupts ci

--- frameworks/hyperoptsklearn/exec.py
+++ frameworks/hyperoptsklearn/exec.py
@@ -78,7 +78,6 @@ def run(dataset, config):
                           before_interrupt=ft.partial(kill_proc_tree, timeout=5, include_parent=False)):
         with Timer() as training:
             estimator.fit(X_train, y_train)
-        log.info(f"Finished fit in {training.duration}s.")
 
     log.info('Predicting on the test set.')
     X_test = dataset.test.X
@@ -90,8 +89,6 @@ def run(dataset, config):
         probabilities = "predictions"  # encoding is handled by caller in `__init__.py`
     else:
         probabilities = None
-    log.info(f"Finished predict in {predict.duration}s.")
-
 
     return result(output_file=config.output_predictions_file,
                   predictions=predictions,
