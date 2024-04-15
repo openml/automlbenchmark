7d74ee663fb95950f0d2fd007139b4c76631612e
PGijsbers <p.gijsbers@tue.nl>
2024-04-15 17:01:27 +0200

    Add mobile navigation bar

--- docs/website/scripts/generate_header.py
+++ docs/website/scripts/generate_header.py
@@ -5,34 +5,30 @@ from typing import NamedTuple
 class NavigationItem(NamedTuple):
     name: str
     url: str
-    icon: str | None = None
+    icon: str
     icon_only: bool = False
 
 
-def generate_desktop_navigation(items: list[NavigationItem]) -> str:
+def generate_navigation_for(items: list[NavigationItem], mobile: bool = False) -> str:
     # TODO: Add outlink icon
     item_template = "<a href=\"URL\" class=\"nav-link nav-icon\">NAME_OR_ICON</a>"
     html_items = []
     for item in items:
-        icon = ""
-        if item.icon and item.icon.endswith(".svg"):
-            with open(item.icon, "r") as fh:
-                icon = fh.read()
+        with open(item.icon, "r") as fh:
+            icon = fh.read()
 
-        name_or_icon = icon if item.icon_only else item.name
+        name_or_icon = icon if (mobile or item.icon_only) else item.name
         nav_html = item_template.replace("URL", item.url).replace("NAME_OR_ICON", name_or_icon)
         html_items.append(nav_html)
 
     items_html = "\n".join(html_items)
+    classes = "mobile nav-mobile" if mobile else "desktop"
     return f"""
-    <nav class="navigation-bar desktop">
+    <nav class="navigation-bar {classes}">
     {items_html}
     </nav>
     """
 
-
-def generate_mobile_navigation(items: list[NavigationItem]) -> str:
-    return ""
 
 def load_navigation_definitions() -> list[NavigationItem]:
     with open("navigation.toml", "rb") as fh:
@@ -42,8 +38,8 @@ def load_navigation_definitions() -> list[NavigationItem]:
 
 def generate_navigation() -> str:
     items = load_navigation_definitions()
-    desktop_navigation = generate_desktop_navigation(items)
-    mobile_navigation = generate_mobile_navigation(items)
+    desktop_navigation = generate_navigation_for(items)
+    mobile_navigation = generate_navigation_for(items, mobile=True)
     return desktop_navigation + mobile_navigation
 
 
