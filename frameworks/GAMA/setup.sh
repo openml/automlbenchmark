f4aa6764c48ae069fd351ca508f72952ade0d2ee
Pieter Gijsbers <p.gijsbers@tue.nl>
2024-06-17 15:02:12 +0200

    Update the GAMA repository location (#627)
    * Update the GAMA repository location
    
    * Fix scikit-learn upperlimit for 23.0.0 release

--- frameworks/GAMA/setup.sh
+++ frameworks/GAMA/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 HERE=$(dirname "$0")
 VERSION=${1:-"stable"}
-REPO=${2:-"https://github.com/PGijsbers/gama"}
+REPO=${2:-"https://github.com/openml-labs/gama"}
 PKG=${3:-"gama"}
 if [[ "$VERSION" == "latest" ]]; then
     VERSION="master"
@@ -21,6 +21,12 @@ else
     rm -Rf ${TARGET_DIR}
     git clone --depth 1 --single-branch --branch ${VERSION} --recurse-submodules ${REPO} ${TARGET_DIR}
     PIP install -U -e ${TARGET_DIR}
+fi
+
+if [[ "$VERSION" == "23.0.0" ]]; then
+    # We include this only because this is the fixed version for the 2023Q2 definition.
+    echo "GAMA/setup.sh: Downgrading scikit-learn to compatible version."
+    PIP install --no-cache-dir -U "scikit-learn<1.3"
 fi
 
 PY -c "from gama import __version__; print(__version__)" >> "${HERE}/.setup/installed"
