ae29fe8a2a7b7fdffedb23298fe26484bbb8d825
PGijsbers <p.gijsbers@tue.nl>
2024-11-20 19:15:27 +0200

    Add tests for is_openml_benchmark

---
+++ tests/unit/amlb/benchmarks/test_openml.py
@@ -0,0 +1,10 @@
+from amlb.benchmarks.openml import is_openml_benchmark
+
+
+def test_is_openml_benchmark_valid():
+    assert is_openml_benchmark("openml/s/271")
+    assert is_openml_benchmark("openml/t/41")
+
+def test_is_openml_benchmark_not_valid():
+    assert not is_openml_benchmark("openml/d/271"), "datasets are not valid targets"
+    assert not is_openml_benchmark("validation"), "this is a file-defined benchmark"
