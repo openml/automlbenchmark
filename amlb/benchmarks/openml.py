3493a2673918c318de0ea803ac873ec443e0ff3e
Pieter Gijsbers <p.gijsbers@tue.nl>
2024-11-24 22:05:15 +0200

    Add tests for OpenML benchmark definitions (#650)
    * Add tests for is_openml_benchmark
    
    * refactor methods slightly for ease of reading
    
    * Add additional cases
    
    * Minor refactoring for ease of reading
    
    * Simplify is_openml_benchmark
    
    * Add tests covering openml benchmark definition loading

--- amlb/benchmarks/openml.py
+++ amlb/benchmarks/openml.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import re
 from typing import cast
 
 import openml
@@ -14,13 +15,7 @@ log = logging.getLogger(__name__)
 
 def is_openml_benchmark(benchmark: str) -> bool:
     """ Check if 'benchmark' is a valid identifier for an openml task or suite. """
-    if len(benchmark.split('/')) == 3:
-        domain, oml_type, oml_id = benchmark.split('/')
-        supported_types = ['s', 't']
-
-        if oml_id.isdecimal():
-            return domain in ("openml", "test.openml") and oml_type in supported_types
-    return False
+    return re.match(r"(openml|test\.openml)/[st]/\d+", benchmark) is not None
 
 
 def load_oml_benchmark(benchmark: str) -> tuple[str, str | None, list[Namespace]]:
@@ -37,29 +32,36 @@ def load_oml_benchmark(benchmark: str) -> tuple[str, str | None, list[Namespace]
         openml.config.set_retry_policy("robot")
 
     if oml_type == 't':
-        log.info("Loading openml task %s.", oml_id)
-        # We first have the retrieve the task because we don't know the dataset id
-        t = openml.tasks.get_task(oml_id, download_data=False, download_qualities=False)
-        data = openml.datasets.get_dataset(t.dataset_id, download_data=False, download_qualities=False)
-        tasks = [Namespace(name=str_sanitize(data.name),
-                           description=data.description,
-                           openml_task_id=t.id,
-                           id="{}.org/t/{}".format(domain, t.id))]
+        tasks = load_openml_task(domain, oml_id)
     elif oml_type == 's':
-        log.info("Loading openml suite %s.", oml_id)
-        suite = openml.study.get_suite(oml_id)
-
-        # Here we know the (task, dataset) pairs so only download dataset meta-data is sufficient
-        tasks = []
-        datasets = cast(pd.DataFrame, openml.datasets.list_datasets(data_id=suite.data, output_format='dataframe'))
-        datasets.set_index('did', inplace=True)
-        for tid, did in zip(cast(list[int], suite.tasks), cast(list[int], suite.data)):
-            tasks.append(Namespace(name=str_sanitize(datasets.loc[did]['name']),
-                                   description=f"{openml.config.server.replace('/api/v1/xml', '')}/d/{did}",
-                                   openml_task_id=tid,
-                                   id="{}.org/t/{}".format(domain, tid)))
+        tasks = load_openml_tasks_from_suite(domain, oml_id)
     else:
         raise ValueError(f"The oml_type is {oml_type} but must be 's' or 't'")
     # The first argument needs to remain parsable further in the pipeline as is
     # The second argument is path, the benchmark does not exist on disk
     return benchmark, None, tasks
+
+
+def load_openml_tasks_from_suite(domain: str, oml_id: str) -> list[Namespace]:
+    log.info("Loading openml suite %s.", oml_id)
+    suite = openml.study.get_suite(oml_id)
+    # Here we know the (task, dataset) pairs so only download dataset meta-data is sufficient
+    tasks = []
+    datasets = cast(pd.DataFrame, openml.datasets.list_datasets(data_id=suite.data, output_format='dataframe'))
+    datasets.set_index('did', inplace=True)
+    for tid, did in zip(cast(list[int], suite.tasks), cast(list[int], suite.data)):
+        tasks.append(Namespace(name=str_sanitize(datasets.loc[did]['name']),
+                               description=f"{domain}/d/{did}",
+                               openml_task_id=tid,
+                               id="{}.org/t/{}".format(domain, tid)))
+    return tasks
+
+def load_openml_task(domain: str, oml_id: str) -> list[Namespace]:
+    log.info("Loading openml task %s.", oml_id)
+    # We first have the retrieve the task because we don't know the dataset id
+    t = openml.tasks.get_task(oml_id, download_data=False, download_qualities=False)
+    data = openml.datasets.get_dataset(t.dataset_id, download_data=False, download_qualities=False)
+    return [Namespace(name=str_sanitize(data.name),
+                       description=data.description,
+                       openml_task_id=t.id,
+                       id="{}.org/t/{}".format(domain, t.id))]
