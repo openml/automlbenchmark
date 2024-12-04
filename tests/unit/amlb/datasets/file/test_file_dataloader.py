5707f2ef4b120d42d3e1d0166b3d7118ed0a9254
Pieter Gijsbers <p.gijsbers@tue.nl>
2024-12-04 20:53:54 +0200

    Remove from_config and only keep from_configs (#659)

--- tests/unit/amlb/datasets/file/test_file_dataloader.py
+++ tests/unit/amlb/datasets/file/test_file_dataloader.py
@@ -6,18 +6,18 @@ import pandas as pd
 import pytest
 import pandas.api.types as pat
 
-from amlb.resources import from_config
+from amlb.resources import from_configs
 from amlb.data import DatasetType
 from amlb.datasets.file import FileLoader
 from amlb.utils import Namespace as ns, path_from_split, split_path
 
 here = os.path.realpath(os.path.dirname(__file__))
-res = os.path.join(here, 'resources')
+res = os.path.join(here, "resources")
 
 
 @pytest.fixture(autouse=True)
 def file_config():
-    return from_config(
+    return from_configs(
         ns(
             input_dir="my_input",
             output_dir="my_output",
@@ -37,7 +37,7 @@ def test_load_binary_task_csv(file_loader):
     ds_def = ns(
         train=os.path.join(res, "kc2_train.csv"),
         test=os.path.join(res, "kc2_test.csv"),
-        target="problems"
+        target="problems",
     )
     ds = file_loader.load(ds_def)
     assert ds.type is DatasetType.binary
@@ -47,13 +47,12 @@ def test_load_binary_task_csv(file_loader):
     _assert_kc2_features(ds, ds_def)
 
 
-
 @pytest.mark.use_disk
 def test_load_binary_task_arff(file_loader):
     ds_def = ns(
         train=os.path.join(res, "kc2_train.arff"),
         test=os.path.join(res, "kc2_test.arff"),
-        target="problems"
+        target="problems",
     )
     ds = file_loader.load(ds_def)
     assert ds.type is DatasetType.binary
@@ -69,20 +68,36 @@ def _assert_kc2_features(dataset, definition):
     target_values = ["no", "yes"]
     _assert_target(dataset.target, name=definition.target, values=target_values)
 
-    assert all([p.data_type in ['int', 'float'] for p in dataset.predictors])
+    assert all([p.data_type in ["int", "float"] for p in dataset.predictors])
     assert all([p.values is None for p in dataset.predictors])
     assert not any([p.is_target for p in dataset.predictors])
     assert not any([p.has_missing_values for p in dataset.predictors])
 
-    floats = [p.name for p in dataset.predictors if p.data_type == 'float']
-    ints = [p.name for p in dataset.predictors if p.data_type == 'int']
-    assert dataset.train.X.dtypes.filter(items=floats).apply(lambda dt: pd.api.types.is_float_dtype(dt)).all()
-    assert dataset.train.X.dtypes.filter(items=ints).apply(lambda dt: pd.api.types.is_integer_dtype(dt)).all()
+    floats = [p.name for p in dataset.predictors if p.data_type == "float"]
+    ints = [p.name for p in dataset.predictors if p.data_type == "int"]
+    assert (
+        dataset.train.X.dtypes.filter(items=floats)
+        .apply(lambda dt: pd.api.types.is_float_dtype(dt))
+        .all()
+    )
+    assert (
+        dataset.train.X.dtypes.filter(items=ints)
+        .apply(lambda dt: pd.api.types.is_integer_dtype(dt))
+        .all()
+    )
     assert pd.api.types.is_categorical_dtype(dataset.train.y.dtypes.iloc[0])
 
     normalize = dataset.target.normalize
-    assert list(normalize(dataset.train.y.squeeze().unique())) == list(normalize(dataset.test.y.squeeze().unique())) == target_values
-    assert list(np.unique(dataset.train.y_enc)) == list(np.unique(dataset.test.y_enc)) == [0, 1]
+    assert (
+        list(normalize(dataset.train.y.squeeze().unique()))
+        == list(normalize(dataset.test.y.squeeze().unique()))
+        == target_values
+    )
+    assert (
+        list(np.unique(dataset.train.y_enc))
+        == list(np.unique(dataset.test.y_enc))
+        == [0, 1]
+    )
 
 
 @pytest.mark.use_disk
@@ -90,7 +105,7 @@ def test_load_multiclass_task_csv(file_loader):
     ds_def = ns(
         train=os.path.join(res, "iris_train.csv"),
         test=os.path.join(res, "iris_test.csv"),
-        target="class"
+        target="class",
     )
     ds = file_loader.load(ds_def)
     assert ds.type is DatasetType.multiclass
@@ -105,10 +120,12 @@ def test_load_multiclass_task_with_num_target_no_type_csv(file_loader):
     ds_def = ns(
         train=os.path.join(res, "iris_num_train.csv"),
         test=os.path.join(res, "iris_num_test.csv"),
-        target="class"
+        target="class",
     )
     ds = file_loader.load(ds_def)
-    assert ds.type is DatasetType.regression, "file loader should detect num target as regression by default"
+    assert (
+        ds.type is DatasetType.regression
+    ), "file loader should detect num target as regression by default"
 
 
 @pytest.mark.use_disk
@@ -117,7 +134,7 @@ def test_load_multiclass_task_with_num_target_csv(file_loader):
         train=os.path.join(res, "iris_num_train.csv"),
         test=os.path.join(res, "iris_num_test.csv"),
         target="class",
-        type="multiclass"
+        type="multiclass",
     )
     ds = file_loader.load(ds_def)
     assert ds.type is DatasetType.multiclass
@@ -132,7 +149,7 @@ def test_load_multiclass_task_arff(file_loader):
     ds_def = ns(
         train=os.path.join(res, "iris_train.arff"),
         test=os.path.join(res, "iris_test.arff"),
-        target="class"
+        target="class",
     )
     ds = file_loader.load(ds_def)
     assert ds.type is DatasetType.multiclass
@@ -145,23 +162,43 @@ def test_load_multiclass_task_arff(file_loader):
 def _assert_iris_features(dataset, definition, num_target=False):
     assert len(dataset.features) == 5
     assert len(dataset.predictors) == 4
-    target_values = ["1", "2", "3"] if num_target else ["iris-setosa", "iris-versicolor", "iris-virginica"]  # values are normalized
+    target_values = (
+        ["1", "2", "3"]
+        if num_target
+        else ["iris-setosa", "iris-versicolor", "iris-virginica"]
+    )  # values are normalized
     _assert_target(dataset.target, name=definition.target, values=target_values)
 
-    assert all([p.data_type in ['int', 'float'] for p in dataset.predictors])
+    assert all([p.data_type in ["int", "float"] for p in dataset.predictors])
     assert all([p.values is None for p in dataset.predictors])
     assert not any([p.is_target for p in dataset.predictors])
     assert not any([p.has_missing_values for p in dataset.predictors])
 
-    floats = [p.name for p in dataset.predictors if p.data_type == 'float']
-    ints = [p.name for p in dataset.predictors if p.data_type == 'int']
-    assert dataset.train.X.dtypes.filter(items=floats).apply(lambda dt: pd.api.types.is_float_dtype(dt)).all()
-    assert dataset.train.X.dtypes.filter(items=ints).apply(lambda dt: pd.api.types.is_integer_dtype(dt)).all()
+    floats = [p.name for p in dataset.predictors if p.data_type == "float"]
+    ints = [p.name for p in dataset.predictors if p.data_type == "int"]
+    assert (
+        dataset.train.X.dtypes.filter(items=floats)
+        .apply(lambda dt: pd.api.types.is_float_dtype(dt))
+        .all()
+    )
+    assert (
+        dataset.train.X.dtypes.filter(items=ints)
+        .apply(lambda dt: pd.api.types.is_integer_dtype(dt))
+        .all()
+    )
     assert pd.api.types.is_categorical_dtype(dataset.train.y.dtypes.iloc[0])
 
     normalize = dataset.target.normalize
-    assert list(normalize(dataset.train.y.squeeze().unique())) == list(normalize(dataset.test.y.squeeze().unique())) == target_values
-    assert list(np.unique(dataset.train.y_enc)) == list(np.unique(dataset.test.y_enc)) == [0, 1, 2]
+    assert (
+        list(normalize(dataset.train.y.squeeze().unique()))
+        == list(normalize(dataset.test.y.squeeze().unique()))
+        == target_values
+    )
+    assert (
+        list(np.unique(dataset.train.y_enc))
+        == list(np.unique(dataset.test.y_enc))
+        == [0, 1, 2]
+    )
 
 
 @pytest.mark.use_disk
@@ -169,7 +206,7 @@ def test_load_regression_task_csv(file_loader):
     ds_def = ns(
         train=os.path.join(res, "cholesterol_train.csv"),
         test=os.path.join(res, "cholesterol_test.csv"),
-        target="chol"
+        target="chol",
     )
     ds = file_loader.load(ds_def)
     assert ds.type is DatasetType.regression
@@ -177,7 +214,7 @@ def test_load_regression_task_csv(file_loader):
     _assert_X_y_types(ds.train)
     _assert_data_consistency(ds)
     _assert_data_paths(ds, ds_def)
-    _assert_cholesterol_features(ds, ds_def, 'csv')
+    _assert_cholesterol_features(ds, ds_def, "csv")
 
 
 @pytest.mark.use_disk
@@ -185,7 +222,7 @@ def test_load_regression_task_arff(file_loader):
     ds_def = ns(
         train=os.path.join(res, "cholesterol_train.arff"),
         test=os.path.join(res, "cholesterol_test.arff"),
-        target="chol"
+        target="chol",
     )
     ds = file_loader.load(ds_def)
     assert ds.type is DatasetType.regression
@@ -193,7 +230,7 @@ def test_load_regression_task_arff(file_loader):
     _assert_X_y_types(ds.train)
     _assert_data_consistency(ds)
     _assert_data_paths(ds, ds_def)
-    _assert_cholesterol_features(ds, ds_def, 'arff')
+    _assert_cholesterol_features(ds, ds_def, "arff")
 
 
 def _assert_cholesterol_features(dataset, definition, fmt):
@@ -201,29 +238,45 @@ def _assert_cholesterol_features(dataset, definition, fmt):
     assert len(dataset.predictors) == 13
     _assert_target(dataset.target, name=definition.target)
 
-    ints = [p.name for p in dataset.predictors if p.data_type == 'int']
-    floats = [p.name for p in dataset.predictors if p.data_type == 'float']
-    categoricals = [p.name for p in dataset.predictors if p.data_type == 'category']
+    ints = [p.name for p in dataset.predictors if p.data_type == "int"]
+    floats = [p.name for p in dataset.predictors if p.data_type == "float"]
+    categoricals = [p.name for p in dataset.predictors if p.data_type == "category"]
 
-    assert len(ints) == (0 if fmt == 'arff' else 6)
-    assert len(floats) == (6 if fmt == 'arff' else 7)
-    assert len(categoricals) == (7 if fmt == 'arff' else 0)
+    assert len(ints) == (0 if fmt == "arff" else 6)
+    assert len(floats) == (6 if fmt == "arff" else 7)
+    assert len(categoricals) == (7 if fmt == "arff" else 0)
     assert not any([p.is_target for p in dataset.predictors])
     assert len([p for p in dataset.predictors if p.has_missing_values]) == 2
 
-    assert dataset.train.X.dtypes.filter(items=ints).apply(lambda dt: pd.api.types.is_integer_dtype(dt)).all()
-    assert dataset.train.X.dtypes.filter(items=floats).apply(lambda dt: pd.api.types.is_float_dtype(dt)).all()
-    assert dataset.train.X.dtypes.filter(items=categoricals).apply(lambda dt: pd.api.types.is_categorical_dtype(dt)).all()
+    assert (
+        dataset.train.X.dtypes.filter(items=ints)
+        .apply(lambda dt: pd.api.types.is_integer_dtype(dt))
+        .all()
+    )
+    assert (
+        dataset.train.X.dtypes.filter(items=floats)
+        .apply(lambda dt: pd.api.types.is_float_dtype(dt))
+        .all()
+    )
+    assert (
+        dataset.train.X.dtypes.filter(items=categoricals)
+        .apply(lambda dt: pd.api.types.is_categorical_dtype(dt))
+        .all()
+    )
     assert pd.api.types.is_float_dtype(dataset.train.y.dtypes.iloc[0])
 
-    assert np.array_equal(dataset.train.y_enc, dataset.train.y.squeeze().to_numpy()), "no encoding should have been applied on regression target"
-    assert np.array_equal(dataset.test.y_enc, dataset.test.y.squeeze().to_numpy()), "no encoding should have been applied on regression target"
+    assert np.array_equal(
+        dataset.train.y_enc, dataset.train.y.squeeze().to_numpy()
+    ), "no encoding should have been applied on regression target"
+    assert np.array_equal(
+        dataset.test.y_enc, dataset.test.y.squeeze().to_numpy()
+    ), "no encoding should have been applied on regression target"
 
 
 def _assert_target(target, name, values=None):
     assert target.name == name
     assert target.values == values
-    assert target.data_type == 'category' if values else 'float'
+    assert target.data_type == "category" if values else "float"
     assert target.is_target
     assert not target.has_missing_values
 
@@ -233,7 +286,7 @@ def _assert_data_paths(dataset, definition):
     assert dataset.test.path == definition.test
     sp = split_path(definition.train)
     fmt = sp.extension[1:]
-    for f in ['arff', 'csv', 'parquet']:
+    for f in ["arff", "csv", "parquet"]:
         if f == fmt:
             assert dataset.train.data_path(f) == dataset.train.path
         else:
@@ -259,15 +312,15 @@ def _assert_data_consistency(dataset, check_encoded=True):
 
     assert not any([p.is_target for p in dataset.predictors])
 
-
     assert dataset.test.X.dtypes.equals(dataset.train.X.dtypes)
     assert dataset.test.y.dtypes.equals(dataset.train.y.dtypes)
 
     if check_encoded:
         assert dataset.train.X_enc.shape == dataset.train.X.shape
         assert np.issubdtype(dataset.train.X_enc.dtype, np.floating)
-        assert np.issubdtype(dataset.train.y_enc.dtype, np.floating)  # not ideal given that it's also for classification targets, but well…
-
+        assert np.issubdtype(
+            dataset.train.y_enc.dtype, np.floating
+        )  # not ideal given that it's also for classification targets, but well…
 
 
 @pytest.mark.use_disk
@@ -297,13 +350,17 @@ def test_load_timeseries_task_csv(file_loader):
     assert pat.is_float_dtype(ds._dtypes[ds.target.name])
 
     # timeseries uses different task schema - set attributes for test to work
-    ds_def['train'] = ds.train.path
-    ds_def['test'] = ds.test.path
+    ds_def["train"] = ds.train.path
+    ds_def["test"] = ds.test.path
     _assert_data_paths(ds, ds_def)
 
 
-@pytest.mark.parametrize("missing_key", ["freq", "forecast_horizon_in_steps", "seasonality"])
-def test_when_timeseries_task_key_is_missing_then_exception_is_raised(file_loader, missing_key):
+@pytest.mark.parametrize(
+    "missing_key", ["freq", "forecast_horizon_in_steps", "seasonality"]
+)
+def test_when_timeseries_task_key_is_missing_then_exception_is_raised(
+    file_loader, missing_key
+):
     task_kwargs = dict(
         path=os.path.join(res, "m4_hourly_subset.csv"),
         forecast_horizon_in_steps=24,
@@ -314,12 +371,17 @@ def test_when_timeseries_task_key_is_missing_then_exception_is_raised(file_loade
     )
     task_kwargs.pop(missing_key)
     ds_def = ns.from_dict(task_kwargs)
-    with pytest.raises(AssertionError, match=f"Task definition for timeseries must include `{missing_key}`"):
+    with pytest.raises(
+        AssertionError,
+        match=f"Task definition for timeseries must include `{missing_key}`",
+    ):
         file_loader.load(ds_def)
 
 
 @pytest.mark.parametrize("missing_key", ["id_column", "timestamp_column"])
-def test_given_nondefault_column_names_when_key_is_missing_then_exception_is_raised(file_loader, missing_key):
+def test_given_nondefault_column_names_when_key_is_missing_then_exception_is_raised(
+    file_loader, missing_key
+):
     task_kwargs = dict(
         path=os.path.join(res, "m4_hourly_subset_nondefault_cols.csv"),
         forecast_horizon_in_steps=24,
@@ -336,7 +398,9 @@ def test_given_nondefault_column_names_when_key_is_missing_then_exception_is_rai
         file_loader.load(ds_def)
 
 
-def test_given_nondefault_column_names_then_timeseries_dataset_can_be_loaded(file_loader):
+def test_given_nondefault_column_names_then_timeseries_dataset_can_be_loaded(
+    file_loader,
+):
     task_kwargs = dict(
         path=os.path.join(res, "m4_hourly_subset_nondefault_cols.csv"),
         forecast_horizon_in_steps=24,
@@ -353,7 +417,9 @@ def test_given_nondefault_column_names_then_timeseries_dataset_can_be_loaded(fil
 
 
 @pytest.mark.parametrize("forecast_horizon, fold", [(50, 2), (100, 0), (10, 9)])
-def test_if_timeseries_dataset_too_short_for_requested_fold_then_exception_is_raised(file_loader, forecast_horizon, fold):
+def test_if_timeseries_dataset_too_short_for_requested_fold_then_exception_is_raised(
+    file_loader, forecast_horizon, fold
+):
     ds_def = ns(
         path=os.path.join(res, "m4_hourly_subset.csv"),
         forecast_horizon_in_steps=forecast_horizon,
@@ -361,5 +427,7 @@ def test_if_timeseries_dataset_too_short_for_requested_fold_then_exception_is_ra
         freq="H",
         type="timeseries",
     )
-    with pytest.raises(ValueError, match="All time series in the dataset must have length"):
+    with pytest.raises(
+        ValueError, match="All time series in the dataset must have length"
+    ):
         file_loader.load(ds_def, fold=fold)
--- tests/unit/amlb/datasets/openml/test_openml_dataloader.py
+++ tests/unit/amlb/datasets/openml/test_openml_dataloader.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from amlb.resources import from_config
+from amlb.resources import from_configs
 from amlb.data import DatasetType
 from amlb.datasets.openml import OpenmlLoader
 from amlb.utils import Namespace as ns
@@ -13,17 +13,13 @@ from amlb.utils import Namespace as ns
 
 @pytest.fixture
 def oml_config():
-    return from_config(
+    return from_configs(
         ns(
             input_dir="my_input",
             output_dir="my_output",
             user_dir="my_user_dir",
             root_dir="my_root_dir",
-
-            openml=ns(
-                apikey="c1994bdb7ecb3c6f3c8f3b35f4b47f1f",
-                infer_dtypes=False
-            )
+            openml=ns(apikey="c1994bdb7ecb3c6f3c8f3b35f4b47f1f", infer_dtypes=False),
         )
     ).config
 
@@ -51,13 +47,15 @@ def _assert_kc2_features(dataset):
 
     _assert_target(dataset.target, "problems", ["no", "yes"])
 
-    assert all([p.data_type == 'number' for p in dataset.predictors])
+    assert all([p.data_type == "number" for p in dataset.predictors])
     assert all([p.values is None for p in dataset.predictors])
     assert not any([p.has_missing_values for p in dataset.predictors])
 
-    assert dataset.train.X.dtypes.apply(lambda dt: pd.api.types.is_numeric_dtype(dt)).all()
-    assert len(dataset.train.X.select_dtypes(include=['float']).columns) == 18
-    assert len(dataset.train.X.select_dtypes(include=['uint8']).columns) == 3
+    assert dataset.train.X.dtypes.apply(
+        lambda dt: pd.api.types.is_numeric_dtype(dt)
+    ).all()
+    assert len(dataset.train.X.select_dtypes(include=["float"]).columns) == 18
+    assert len(dataset.train.X.select_dtypes(include=["uint8"]).columns) == 3
     assert pd.api.types.is_categorical_dtype(dataset.train.y.dtypes.iloc[0])
 
 
@@ -77,13 +75,17 @@ def _assert_iris_features(dataset):
     assert len(dataset.features) == 5
     assert len(dataset.predictors) == 4
 
-    _assert_target(dataset.target, "class", ["iris-setosa", "iris-versicolor", "iris-virginica"])
+    _assert_target(
+        dataset.target, "class", ["iris-setosa", "iris-versicolor", "iris-virginica"]
+    )
 
-    assert all([p.data_type == 'number' for p in dataset.predictors])
+    assert all([p.data_type == "number" for p in dataset.predictors])
     assert all([p.values is None for p in dataset.predictors])
     assert not any([p.has_missing_values for p in dataset.predictors])
 
-    assert dataset.train.X.dtypes.apply(lambda dt: pd.api.types.is_float_dtype(dt)).all()
+    assert dataset.train.X.dtypes.apply(
+        lambda dt: pd.api.types.is_float_dtype(dt)
+    ).all()
     assert pd.api.types.is_categorical_dtype(dataset.train.y.dtypes.iloc[0])
 
 
@@ -105,33 +107,53 @@ def _assert_cholesterol_features(dataset):
 
     _assert_target(dataset.target, "chol")
 
-    numericals = [p.name for p in dataset.predictors if p.data_type == 'number']
-    categoricals = [p.name for p in dataset.predictors if p.data_type == 'category']
+    numericals = [p.name for p in dataset.predictors if p.data_type == "number"]
+    categoricals = [p.name for p in dataset.predictors if p.data_type == "category"]
     assert len(numericals) == 6
     assert len(categoricals) == 7
     assert len([p for p in dataset.predictors if p.has_missing_values]) == 2
 
-    assert dataset.train.X.dtypes.filter(items=numericals).apply(lambda dt: pd.api.types.is_numeric_dtype(dt)).all()
-    assert dataset.train.X.dtypes.filter(items=categoricals).apply(lambda dt: pd.api.types.is_categorical_dtype(dt)).all()
-    assert len(dataset.train.X.select_dtypes(include=['float']).columns) == 6
+    assert (
+        dataset.train.X.dtypes.filter(items=numericals)
+        .apply(lambda dt: pd.api.types.is_numeric_dtype(dt))
+        .all()
+    )
+    assert (
+        dataset.train.X.dtypes.filter(items=categoricals)
+        .apply(lambda dt: pd.api.types.is_categorical_dtype(dt))
+        .all()
+    )
+    assert len(dataset.train.X.select_dtypes(include=["float"]).columns) == 6
     assert pd.api.types.is_float_dtype(dataset.train.y.dtypes.iloc[0])
 
 
 def _assert_target(target, name, values=None):
     assert target.name == name
     assert target.values == values
-    assert target.data_type == 'category' if values else 'number'
+    assert target.data_type == "category" if values else "number"
     assert target.is_target
     assert not target.has_missing_values
 
 
 def _assert_data_paths(dataset, ds_id, fold):
-    assert dataset.train.path.endswith(os.path.join("datasets", str(ds_id), f"dataset_train_{fold}.arff"))
-    assert dataset.test.path.endswith(os.path.join("datasets", str(ds_id), f"dataset_test_{fold}.arff"))
-    assert dataset.train.data_path('csv').endswith(os.path.join("datasets", str(ds_id), f"dataset_train_{fold}.csv"))
-    assert dataset.test.data_path('csv').endswith(os.path.join("datasets", str(ds_id), f"dataset_test_{fold}.csv"))
-    assert dataset.train.data_path('parquet').endswith(os.path.join("datasets", str(ds_id), f"dataset_train_{fold}.parquet"))
-    assert dataset.test.data_path('parquet').endswith(os.path.join("datasets", str(ds_id), f"dataset_test_{fold}.parquet"))
+    assert dataset.train.path.endswith(
+        os.path.join("datasets", str(ds_id), f"dataset_train_{fold}.arff")
+    )
+    assert dataset.test.path.endswith(
+        os.path.join("datasets", str(ds_id), f"dataset_test_{fold}.arff")
+    )
+    assert dataset.train.data_path("csv").endswith(
+        os.path.join("datasets", str(ds_id), f"dataset_train_{fold}.csv")
+    )
+    assert dataset.test.data_path("csv").endswith(
+        os.path.join("datasets", str(ds_id), f"dataset_test_{fold}.csv")
+    )
+    assert dataset.train.data_path("parquet").endswith(
+        os.path.join("datasets", str(ds_id), f"dataset_train_{fold}.parquet")
+    )
+    assert dataset.test.data_path("parquet").endswith(
+        os.path.join("datasets", str(ds_id), f"dataset_test_{fold}.parquet")
+    )
 
 
 def _assert_X_y_types(data_split):
@@ -155,6 +177,7 @@ def _assert_data_consistency(dataset):
     assert dataset.test.X.dtypes.equals(dataset.train.X.dtypes)
     assert dataset.test.y.dtypes.equals(dataset.train.y.dtypes)
 
-    assert np.issubdtype(dataset.train.X_enc.dtype, np.floating)   # all categorical features are directly encoded as float
+    assert np.issubdtype(
+        dataset.train.X_enc.dtype, np.floating
+    )  # all categorical features are directly encoded as float
     assert np.issubdtype(dataset.train.y_enc.dtype, np.floating)
-
