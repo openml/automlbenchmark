bd60b975f2e97e303e1c0af7a2326817bba1bdc9
PGijsbers <p.gijsbers@tue.nl>
2025-02-27 22:17:20 +0200

    Run pre-commit

--- frameworks/FEDOT/exec.py
+++ frameworks/FEDOT/exec.py
@@ -13,11 +13,13 @@ log = logging.getLogger(__name__)
 def run(dataset, config):
     log.info("\n**** FEDOT ****\n")
 
-    is_classification = config.type == 'classification'
+    is_classification = config.type == "classification"
     scoring_metric = get_fedot_metrics(config)
 
     training_params = {"preset": "best_quality", "n_jobs": config.cores}
-    training_params.update({k: v for k, v in config.framework_params.items() if not k.startswith('_')})
+    training_params.update(
+        {k: v for k, v in config.framework_params.items() if not k.startswith("_")}
+    )
     n_jobs = training_params["n_jobs"]
 
     log.info(f"Running FEDOT with a maximum time of {config.max_runtime_seconds}s on {n_jobs} cores, \
@@ -62,15 +64,15 @@ def run(dataset, config):
 
 def get_fedot_metrics(config):
     metrics_mapping = dict(
-        acc='accuracy',
-        auc='roc_auc',
-        f1='f1',
-        logloss='neg_log_loss',
-        mae='mae',
-        mse='mse',
-        msle='msle',
-        r2='r2',
-        rmse='rmse',
+        acc="accuracy",
+        auc="roc_auc",
+        f1="f1",
+        logloss="neg_log_loss",
+        mae="mae",
+        mse="mse",
+        msle="msle",
+        r2="r2",
+        rmse="rmse",
     )
     scoring_metric = metrics_mapping.get(config.metric, None)
 
