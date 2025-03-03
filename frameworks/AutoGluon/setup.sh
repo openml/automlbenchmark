13697c2fb4c1846c7973e0d58b30c51cecae196f
Pieter Gijsbers <p.gijsbers@tue.nl>
2025-03-03 20:23:48 +0200

    Reduce frameworks in common check & fix AutoGluon setup (#696)
    * Drastically reduce the amount of frameworks used in common check
    
    To improve the consistency of CI, for now we focus on just three
    reliable AutoML frameworks.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix syntax
    
    * Remove AutoGluon from baselines
    
    * Add optional dependency to force numpy downgrade
    
    * Install skex dependency when running on intel architecture
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -31,12 +31,20 @@ PIP install --upgrade pip
 PIP install uv
 UV="${PY_EXEC_NO_ARGS} -m uv"
 
+ARCH=$(uname -m)
+if [[ ${ARCH} == "x86_64" ]]; then
+    DEPENDENCIES='all,skex'
+else
+    DEPENDENCIES='all'
+    echo "Skipping 'skex' install as architecture is not 64-bit intel."
+fi
+
 if [[ "$VERSION" == "stable" ]]; then
     $UV pip install --no-cache-dir -U "${PKG}"
-    $UV pip install --no-cache-dir -U "${PKG}.tabular[skex]"
+    $UV pip install --no-cache-dir -U "${PKG}.tabular[${DEPENDENCIES}]"
 elif [[ "$VERSION" =~ ^[0-9] ]]; then
     $UV pip install --no-cache-dir -U "${PKG}==${VERSION}"
-    $UV pip install --no-cache-dir -U "${PKG}.tabular[skex]==${VERSION}"
+    $UV pip install --no-cache-dir -U "${PKG}.tabular[${DEPENDENCIES}]==${VERSION}"
 else
     TARGET_DIR="${HERE}/lib/${PKG}"
     rm -Rf ${TARGET_DIR}
@@ -46,7 +54,7 @@ else
 
     # Install in non-editable mode to avoid interaction with other pre-existing AutoGluon installations
     env PATH="$PY_EXEC_DIR:$PATH" bash -c "./full_install.sh --non-editable"
-    $UV pip install tabular/[skex]
+    $UV pip install tabular/[${DEPENDENCIES}]
 fi
 
 # Note: `setuptools` being present in the venv will cause torch==1.4.x to raise an exception for an unknown reason in AMLB.
