df114727424821f73734da80e9124a0e26b5a69d
PGijsbers <p.gijsbers@tue.nl>
2025-03-10 21:27:47 +0200

    Switch to functools implementation which introduces 'bug'
    The bug is that now objects of the same class can not have their
    cache cleared independently. From the usage, I do not believe that
    there would otherwise be two different instances of the same class
    anyway, though I may be wrong (thinking datasplit). Will need
    to investigate more tomorrow.
    
    In any case, the plan is to move to a more friendly implementation
    anyway where those classes that do need cache eviction simply add
    a two-liner for that.

--- amlb/utils/cache.py
+++ amlb/utils/cache.py
@@ -1,6 +1,6 @@
 import logging
+import functools
 
-from .core import flatten
 
 log = logging.getLogger(__name__)
 
@@ -13,78 +13,38 @@ def _cached_property_name(fn):
     return _CACHE_PROP_PREFIX_ + (fn.__name__ if hasattr(fn, "__name__") else str(fn))
 
 
-def clear_cache(self, functions=None):
-    cached_properties = [
-        prop for prop in dir(self) if prop.startswith(_CACHE_PROP_PREFIX_)
-    ]
-    properties_to_clear = (
-        cached_properties
-        if functions is None
-        else [
-            prop
-            for prop in [_cached_property_name(fn) for fn in functions]
-            if prop in cached_properties
-        ]
-    )
-    for prop in properties_to_clear:
-        delattr(self, prop)
-    log.debug("Cleared cached properties: %s.", properties_to_clear)
-
-
-def cache(self, key, fn):
-    """
-
-    :param self: the object that will hold the cached value
-    :param key: the key/attribute for the cached value
-    :param fn: the function returning the value to be cached
-    :return: the value returned by fn on first call
-    """
-    if not hasattr(self, key):
-        value = fn(self)
-        setattr(self, key, value)
-    return getattr(self, key)
+def clear_cache(obj, functions=None):
+    attributes = {att: getattr(obj, att) for att in (functions or dir(obj.__class__))}
+    functions = {
+        name: fn for name, fn in attributes.items() if callable(getattr(obj, name))
+    }
+    properties = {
+        name: getattr(obj.__class__, name).fget
+        for name, prop in attributes.items()
+        if isinstance(getattr(obj.__class__, name), property)
+    }
+    # Note that it is not possible to evict specific entries from the lru cache,
+    # this means that the property and methods will be cleared for *all* objects
+    # obj's class. In practice, doesn't really come into play since clear_cached
+    # is only called on Dataset and Datasplit objects.
+    cleared_properties = []
+    for name, fn in (functions | properties).items():
+        if hasattr(fn, "cache_clear"):
+            fn.cache_clear()
+            cleared_properties.append(name)
+    log.debug("Cleared cached properties: %s.", cleared_properties)
 
 
 def cached(fn):
-    """
-
-    :param fn:
-    :return:
-    """
-    result = _cached_property_name(fn)
-
-    def decorator(self):
-        return cache(self, result, fn)
-
-    return decorator
+    return functools.cache(fn)
 
 
 def memoize(fn):
-    prop_name = _cached_property_name(fn)
-
-    def decorator(self, *args, **kwargs):
-        memo = cache(self, prop_name, lambda _: {})
-        key = tuple(flatten((args, kwargs), flatten_dict=True))
-        if key not in memo:
-            memo[key] = fn(self, *args, **kwargs)
-        return memo[key]
-
-    return decorator
+    return functools.cache(fn)
 
 
 def lazy_property(prop_fn):
-    """
-
-    :param prop_fn:
-    :return:
-    """
-    prop_name = _cached_property_name(prop_fn)
-
-    @property
-    def decorator(self):
-        return cache(self, prop_name, prop_fn)
-
-    return decorator
+    return property(functools.cache(prop_fn))
 
 
 __all__ = [s for s in dir() if not s.startswith("_") and s not in __no_export]
