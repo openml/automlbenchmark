92fce343442e4f94a6da491c82ea6f6b8606d5f9
PGijsbers <p.gijsbers@tue.nl>
2025-03-11 22:28:08 +0200

    Properly clear properties and also functions
    Still has the issue that cache for all instances of the class are
    cleared for functions (but not for properties).

--- amlb/utils/cache.py
+++ amlb/utils/cache.py
@@ -11,15 +11,31 @@ __no_export = set(dir())  # all variables defined above this are not exported
 
 def clear_cache(obj: Any, functions: Sequence[str] | None = None) -> None:
     attributes_to_check = functions or dir(type(obj))
+    # Must be careful to check the definitions on the class, checking on the instance
+    # will trigger invocations of the cached properties through `getattr`.
     cached_properties = [
         name
         for name in attributes_to_check
         if isinstance(getattr(type(obj), name), cached_property)
     ]
-    for property_ in cached_properties:
-        delattr(obj, property_)
+    _functions = [fn for fn in attributes_to_check if callable(getattr(type(obj), fn))]
 
-    log.debug("Cleared cached properties: %s.", cached_properties)
+    cleared_properties = []
+    for property_ in cached_properties:
+        # You need to delete the attribute to evict the cache of a cached property,
+        # but you cannot check for it with hasattr as that would invoke it.
+        try:
+            delattr(obj, property_)
+            cleared_properties.append(property_)
+        except AttributeError:
+            pass
+
+    for cached_function in [getattr(obj, fn) for fn in _functions]:
+        if hasattr(cached_function, "cache_clear"):
+            cached_function.cache_clear()
+            cleared_properties.append(cached_function.__name__)
+
+    log.debug("Cleared cached properties: %s.", cleared_properties)
 
 
 def cached(fn):
