2759e2ef82f9a323c70d76bf3be3f2f3594861b3
PGijsbers <p.gijsbers@tue.nl>
2025-06-10 21:32:53 +0200

    Remove pickle serialization tests

--- tests/unit/amlb/utils/serialization/test_serializers.py
+++ tests/unit/amlb/utils/serialization/test_serializers.py
@@ -19,35 +19,11 @@ def test_serialize_list_json(tmpdir):
 
 
 @pytest.mark.use_disk
-def test_serialize_list_pickle(tmpdir):
-    li = [[1, 2.2, None, 3, 4.4, "foo", True], ["bar", False, 2 / 3]]
-    dest = os.path.join(tmpdir, "my_list")
-    path = serialize_data(li, dest, config=ns(fallback_serializer="pickle"))
-    assert path == f"{dest}.pkl"
-
-    reloaded = deserialize_data(path)
-    assert isinstance(reloaded, list)
-    assert li == reloaded
-
-
-@pytest.mark.use_disk
 def test_serialize_dict_json(tmpdir):
     di = dict(first=[1, 2.2, None, 3, 4.4, "foo", True], second=["bar", False, 2 / 3])
     dest = os.path.join(tmpdir, "my_dict")
     path = serialize_data(di, dest, config=ns(fallback_serializer="json"))
     assert path == f"{dest}.json"
-
-    reloaded = deserialize_data(path)
-    assert isinstance(reloaded, dict)
-    assert di == reloaded
-
-
-@pytest.mark.use_disk
-def test_serialize_dict_pickle(tmpdir):
-    di = dict(first=[1, 2.2, None, 3, 4.4, "foo", True], second=["bar", False, 2 / 3])
-    dest = os.path.join(tmpdir, "my_dict")
-    path = serialize_data(di, dest, config=ns(fallback_serializer="pickle"))
-    assert path == f"{dest}.pkl"
 
     reloaded = deserialize_data(path)
     assert isinstance(reloaded, dict)
@@ -154,83 +130,6 @@ def test_serialize_sparse_matrix_reload_as_array(tmpdir):
     )
     assert isinstance(reloaded, np.ndarray)
     assert np.array_equal(mat.toarray(), reloaded, equal_nan=True)
-
-
-@pytest.mark.use_disk
-def test_serialize_sparse_dataframe(tmpdir):
-    import pandas as pd
-
-    ser_config = ns(
-        pandas_serializer="pickle", sparse_dataframe_deserialized_format=None
-    )
-    dfs = pd.DataFrame(
-        dict(
-            first=[0, 0, 0, 3.3],
-            second=[4.4, 0, 0, 0],
-            third=[0, pd.NA, 0, 0],
-        )
-    ).astype("Sparse")
-    assert is_sparse(dfs)
-    dest = os.path.join(tmpdir, "my_sparse_df")
-    path = serialize_data(dfs, dest, config=ser_config)
-    assert path == f"{dest}.pd"
-
-    reloaded = deserialize_data(path, config=ser_config)
-    assert isinstance(reloaded, pd.DataFrame)
-    assert is_sparse(reloaded)
-    assert dfs.compare(reloaded).empty
-
-
-@pytest.mark.use_disk
-def test_serialize_pandas_dataframe_reload_as_dense(tmpdir):
-    import pandas as pd
-
-    ser_config = ns(
-        pandas_serializer="pickle", sparse_dataframe_deserialized_format="dense"
-    )
-    dfs = pd.DataFrame(
-        dict(
-            first=[0, 0, 0, 3.3],
-            second=[4.4, 0, 0, 0],
-            third=[0, pd.NA, 0, 0],
-            # fourth=[None, None, 'a', None]
-        )
-    ).astype("Sparse")
-    assert is_sparse(dfs)
-    dest = os.path.join(tmpdir, "my_sparse_df")
-    path = serialize_data(dfs, dest, config=ser_config)
-    assert path == f"{dest}.pd"
-
-    reloaded = deserialize_data(path, config=ser_config)
-    assert isinstance(reloaded, pd.DataFrame)
-    assert not is_sparse(reloaded)
-    assert dfs.compare(reloaded).empty
-
-
-@pytest.mark.use_disk
-def test_serialize_pandas_dataframe_reload_as_array(tmpdir):
-    import numpy as np
-    import pandas as pd
-
-    ser_config = ns(
-        pandas_serializer="pickle", sparse_dataframe_deserialized_format="array"
-    )
-    dfs = pd.DataFrame(
-        dict(
-            first=[0, 0, 0, 3.3],
-            second=[4.4, 0, 0, 0],
-            third=[0, pd.NA, 0, 0],
-            # fourth=[None, None, 'a', None]
-        )
-    ).astype("Sparse")
-    assert is_sparse(dfs)
-    dest = os.path.join(tmpdir, "my_sparse_df")
-    path = serialize_data(dfs, dest, config=ser_config)
-    assert path == f"{dest}.pd"
-
-    reloaded = deserialize_data(path, config=ser_config)
-    assert isinstance(reloaded, np.ndarray)
-    assert np.array_equal(dfs.to_numpy(), np.asarray(reloaded), equal_nan=True)
 
 
 @pytest.mark.use_disk
